66b95eff27abb5576bdfb10ea8f8ac1c13f7c59a
PimVeefkind <71269094+PimVeefkind@users.noreply.github.com>
2021-12-11 18:11:15 +0100

    verander_gegevens

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -23,19 +23,21 @@ class MePage extends StatelessWidget {
             child: profile_picture(),
           ),
           SizedBox(height: 20),
-          Information_field('Naam', 'Pim Veefkind'),
-          Information_field('Lidnummer', '18257'),
-          Information_field('Geboortedatum', '25-03-2000'),
-          Information_field('Telefoonnummer', '0629110215'),
-          Information_field('E-mailadres', 'pim.veefkind@gmail.com'),
-          Information_field('Adres', 'Schubertlaan 201, 2324CT Leiden'),
-          Information_field('Jaar van aankomst', '2018'),
-          Information_field('Ploeg', 'Clavis'),
-          Information_field('Studie', 'Natuurkunde'),
-          Information_field('IBAN', 'NL19 AWRD 8943 1193 10'),
-          Information_field('Dubbellid', 'Nee'),
-          Information_field('Aantal blikken', '0'),
-          Information_field('Aantal taarten', '0'),
+          display_static_information('Naam', 'Pim Veefkind'),
+          display_static_information('Lidnummer', '18257'),
+          display_static_information('Geboortedatum', '25-03-2000'),
+          display_amendable_information('Telefoonnummer', '0629110215'),
+          display_amendable_information(
+              'E-mailadres', 'pim.veefkind@gmail.com'),
+          display_amendable_information(
+              'Adres', 'Schubertlaan 201, 2324CT Leiden'),
+          display_static_information('Jaar van aankomst', '2018'),
+          display_amendable_information('Ploeg', 'Clavis'),
+          display_amendable_information('Studie', 'Natuurkunde'),
+          display_amendable_information('IBAN', 'NL19 AWRD 8943 1193 10'),
+          display_amendable_information('Dubbellid', 'Nee'),
+          display_amendable_information('Aantal blikken', '0'),
+          display_amendable_information('Aantal taarten', '0'),
         ]));
   }
 }
@@ -62,11 +64,11 @@ class profile_picture extends StatelessWidget {
   }
 }
 
-class Information_field extends StatelessWidget {
+class display_amendable_information extends StatelessWidget {
   final String info_kind;
   final String user_info;
 
-  Information_field(this.info_kind, this.user_info);
+  display_amendable_information(this.info_kind, this.user_info);
 
   @override
   Widget build(BuildContext context) {
@@ -86,12 +88,147 @@ class Information_field extends StatelessWidget {
             ],
           ),
         ),
-        const Icon(Icons.edit, color: Colors.grey, size: 20),
+        IconButton(
+            iconSize: 20,
+            icon: Icon(Icons.edit, color: Colors.grey),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => change_information()));
+            }),
       ]),
       const Divider(
         color: Colors.grey,
         thickness: 1,
       ),
     ]);
+  }
+}
+
+class display_static_information extends StatelessWidget {
+  final String info_kind;
+  final String user_info;
+
+  display_static_information(this.info_kind, this.user_info);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          info_kind,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11),
+        ),
+        SizedBox(height: 3),
+        Text(user_info),
+        const Divider(
+          color: Colors.grey,
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+}
+
+class change_information extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const AppIconWidget(image: Images.appLogo),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(30),
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  header_change_information(),
+                  const SizedBox(height: 40),
+                  text_field_change_information(),
+                  const SizedBox(height: 60),
+                  buttons_change_information(),
+                  const SizedBox(height: 20)
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+          const Text(
+              'Aanpassingen worden ook doorgevoerd in de algemene administratie. Hierom moet eerst toestemming worden gegeven voor een verandering. Het kan even duren voordat de verandering zichtbaar is.'),
+        ],
+      ),
+    );
+  }
+}
+
+class header_change_information extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: const Text('Aanpassen Gegeven',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+    );
+  }
+}
+
+class text_field_change_information extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: TextField(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          labelText: 'Nieuw gegeven',
+        ),
+      ),
+    );
+  }
+}
+
+class buttons_change_information extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          color: Colors.red,
+          child: TextButton(
+            child: const Text('Annuleren',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16)),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        Container(
+          color: Colors.green,
+          child: TextButton(
+            child: const Text('Bevestigen',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16)),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
