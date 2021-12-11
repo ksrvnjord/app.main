7d86ab23b309a1a38c384145bcf6d1396bf4c2b4
PimVeefkind <71269094+PimVeefkind@users.noreply.github.com>
2021-12-11 15:31:55 +0100

    Update user_page.dart

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ksrv_njord_app/assets/images.dart';
+import 'package:flutter/src/material/colors.dart';
 import 'package:ksrv_njord_app/widgets/app_icon_widget.dart';
 
 double betweenFields = 20;
-double marginContainer = 30;
-double paddingBody = 30;
+double marginContainer = 5;
+double paddingBody = 15;
 
 class MePage extends StatelessWidget {
   const MePage({Key? key}) : super(key: key);
@@ -16,60 +17,81 @@ class MePage extends StatelessWidget {
         appBar: AppBar(
           title: const AppIconWidget(image: Images.appLogo),
         ),
-        body: Padding(
-            // add padding to entire body
-            padding: EdgeInsets.all(paddingBody),
-            child: Center(
-                child: Column(
-              children: <Widget>[
-                Container(
-                    child: Text('Ruimte voor profielfoto'),
-                    margin: EdgeInsets.all(marginContainer),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 3,
-                        ))),
-                Information('Naam:', 'Pim'),
-                SizedBox(height: betweenFields),
-                Information('e-mail:', 'Pim@gmail.com'),
-                SizedBox(height: betweenFields),
-                Information('Telefoonnummer:', '02589374'),
-              ],
-            )
-          )
-        )
-      );
-    }
+        body: ListView(padding: EdgeInsets.all(paddingBody), children: <Widget>[
+          SizedBox(height: 10),
+          Center(
+            child: profile_picture(),
+          ),
+          SizedBox(height: 20),
+          Information_field('Naam', 'Pim Veefkind'),
+          Information_field('Lidnummer', '18257'),
+          Information_field('Geboortedatum', '25-03-2000'),
+          Information_field('Telefoonnummer', '0629110215'),
+          Information_field('E-mailadres', 'pim.veefkind@gmail.com'),
+          Information_field('Adres', 'Schubertlaan 201, 2324CT Leiden'),
+          Information_field('Jaar van aankomst', '2018'),
+          Information_field('Ploeg', 'Clavis'),
+          Information_field('Studie', 'Natuurkunde'),
+          Information_field('IBAN', 'NL19 AWRD 8943 1193 10'),
+          Information_field('Dubbellid', 'Nee'),
+          Information_field('Aantal blikken', '0'),
+          Information_field('Aantal taarten', '0'),
+        ]));
   }
+}
 
-class Information extends StatelessWidget {
-  final String info_kind;
-  final String user_info;
-
-  const Information(this.info_kind, this.user_info);
-
+class profile_picture extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Row(children: [
-          Expanded(child: Text(info_kind)),
-          Expanded(
-            child:
-                Align(alignment: Alignment.centerRight, child: Text(user_info)),
+      width: 150,
+      height: 150,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+      ),
+      child: Expanded(
+        child: FittedBox(
+          fit: BoxFit.fill,
+          child: const Icon(
+            Icons.account_circle_rounded,
+            color: Colors.grey,
           ),
-        ]),
-        width: 280,
-        padding: EdgeInsets.only(left: 5, bottom: 3, right: 5, top: 3),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            border: Border.all(
-              color: Colors.lightBlue,
-              width: 2,
-            )));
+        ),
+      ),
+    );
+  }
+}
+
+class Information_field extends StatelessWidget {
+  final String info_kind;
+  final String user_info;
+
+  Information_field(this.info_kind, this.user_info);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                info_kind,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11),
+              ),
+              SizedBox(height: 3),
+              Text(user_info),
+            ],
+          ),
+        ),
+        const Icon(Icons.edit, color: Colors.grey, size: 20),
+      ]),
+      const Divider(
+        color: Colors.grey,
+        thickness: 1,
+      ),
+    ]);
   }
 }
