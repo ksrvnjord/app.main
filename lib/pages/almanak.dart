d641e322193269821d75aa302e153da4f972ab82
KristieW <kristiewesdorp@icloud.com>
2021-12-29 12:23:28 +0100

    Converting the AlmanakPage widget to a stateful widget, and adding the user names as list tiles.

--- lib/pages/almanak.dart
+++ lib/pages/almanak.dart
@@ -1,32 +1,46 @@
 import 'package:flutter/material.dart';
 
-var voorbeeldNamen = ['Boone', // We want to get this info from the database.
-                      'Kristie',
-  'Veefkind'];
+class AlmanakPage extends StatefulWidget {
+  const AlmanakPage({Key? key}) : super(key: key);
 
-class AlmanakPage extends StatelessWidget { // What kind of widget should this be?
-  const AlmanakPage({Key? key}) : super(key: key); // Is this something we need for navigation?
+  @override
+  State<StatefulWidget> createState() => _AlmanakPageState();
+}
 
-  // We want a page
-  // Appbar like the  main  one,  stating that we are in the almanak
-    // Text widget 'almanak'
-  //    On it should be a search bar
-  //    And a list of the users. In the form of a button? onPressed for more info.
+class _AlmanakPageState extends State<AlmanakPage> {
 
-  @override // Decorator: to make the code clearer / cleaner. We override the build method
+  var voorbeeldNamen = ['Boone', // We want to get this info from the database.
+    'Kristie',
+    'Veefkind'];
+
+  // Add search bar widget.
+  // Get info from database instead of example list.
+  // Construct all names in the app automatically.
+
+  @override
   Widget build(BuildContext context){
     return MaterialApp( // Or padding / Card
       title: "Almanak",
       home: Scaffold(
         appBar: AppBar(
           title: Text('Almanak'),
+          backgroundColor: Colors.lightBlue,
+          shadowColor: Colors.transparent,
         ),
           body: Column(
-            children: [
-              Text(voorbeeldNamen[0]), // Hiervan een for loop maken? Handiger opschrijven.
-              Text(voorbeeldNamen[1]),
-              Text(voorbeeldNamen[2]),
-
+            children: [ // For loop maken voor ListTiles (of beter alternatief)
+              ListTile(
+                title: Text(voorbeeldNamen[0]),
+                onTap: () {print('This is Max Boone.');},
+              ),
+              ListTile(
+                title: Text(voorbeeldNamen[1]),
+                onTap: () {print('This is Kristie Wesdorp.');},
+              ),
+              ListTile(
+                title: Text(voorbeeldNamen[2]),
+                onTap: () {print('This is Pim Veefkind.');},
+              ),
             ],)
       )
     );
