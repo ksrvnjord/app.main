05f744116d91b3cbce2a4608d9bd6236ee023de3
KristieW <kristiewesdorp@icloud.com>
2021-12-30 00:30:42 +0100

    Fixed the display of the names.

--- lib/pages/almanak.dart
+++ lib/pages/almanak.dart
@@ -10,51 +10,45 @@ class AlmanakPage extends StatefulWidget {
 
 class _AlmanakPageState extends State<AlmanakPage> {
 
-  List<String> voorbeeldNamen = ['Boone', // We want to get this info from the database.
+  List<String> voorbeeldLeden = ['Boone', // Replace with info from the database.
     'Kristie',
     'Veefkind'];
 
-  // Get info from database instead of example list.
-  // Construct all names in the app automatically.
-
   @override
   Widget build(BuildContext context){
-    return MaterialApp( // Or padding / Card
+    return MaterialApp(
       title: "Almanak",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-            Text('Almanak'),
-            IconButton( // Put in the title for now, needs styling later.
-              onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(voorbeeldNamen), // Give the possible search terms as parameter to the search bar function.
-                );
-              },
-              icon: const Icon(Icons.search), // Use classic search icon.
+      home: Builder( // Wrap in a builder widget to get the right context for showSearch.
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Text('Almanak'),
+                IconButton( // Put in the title for now, needs styling later.
+                  onPressed: () {
+                    showSearch(
+                      context: context,
+                      delegate: CustomSearchDelegate(voorbeeldLeden), // Give the possible search terms as parameter to the search bar function.
+                    );
+                  },
+                  icon: const Icon(Icons.search), // Use classic search icon.
+                ),
+              ],
             ),
-            ],
+            backgroundColor: Colors.lightBlue,
+            shadowColor: Colors.transparent,
           ),
-          backgroundColor: Colors.lightBlue,
-          shadowColor: Colors.transparent,
-        ),
-          body: Column(
-            children: [ // Make for loop.
-              ListTile(
-                title: Text(voorbeeldNamen[0]),
-                onTap: () {print('This is Max Boone.');},
-              ),
-              ListTile(
-                title: Text(voorbeeldNamen[1]),
-                onTap: () {print('This is Kristie Wesdorp.');},
-              ),
-              ListTile(
-                title: Text(voorbeeldNamen[2]),
-                onTap: () {print('This is Pim Veefkind.');},
-              ),
-            ],)
+          body: ListView.builder(
+            itemCount: voorbeeldLeden.length,
+            itemBuilder: (context, index) {
+              final lid = voorbeeldLeden[index];
+              return ListTile(
+                title: Text(lid),
+                onTap: () {print(lid);}, // Replace with more info about the lid.
+              );
+              },
+          ),
+        )
       )
     );
   }
