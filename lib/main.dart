5d84ebfe41dc158cc28eb190f3faddd8158115e4
Max Boone <boonem@transip.nl>
2021-12-30 12:36:27 +0100

    Add Sentry for exception logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ksrv_njord_app/providers/authentication.dart';
 import 'package:ksrv_njord_app/screens/main.dart';
 import 'package:ksrv_njord_app/screens/login.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+Future<void> main() async {
+  if (kReleaseMode) {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://d45c56c8f63a498188d63af3c1cf585d@sentry.ksrv.nl/3';
+      },
+      appRunner: () => runApp(const ProviderScope(child: MyApp())),
+    );
+  } else {
+    runApp(const ProviderScope(child: MyApp()));
+  }
 }
 
 class MyApp extends HookConsumerWidget {
