01b6e422a3dd8d52afe4446acb908590a3ce0e10
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-01-14 21:04:29 +0100

    3 announcements are displayed on home page
    Altered announcements to make it a widget to make it more modular

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:ksrvnjord_main_app/widgets/general/vaarverbod.dart';
 import 'package:ksrvnjord_main_app/widgets/images/bar_logo.dart';
+import 'package:ksrvnjord_main_app/pages/announcements.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,15 +11,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const BarLogoWidget(image: Images.appLogo),
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.lightBlue,
-            shadowColor: Colors.transparent,
-            systemOverlayStyle:
-                const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue)),
-        body: Center(
-          child: Column(children: const [VaarverbodWidget()]),
-        ));
+      appBar: AppBar(
+          title: const BarLogoWidget(image: Images.appLogo),
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.lightBlue,
+          shadowColor: Colors.transparent,
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue)),
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Center(child: Column(children: const [VaarverbodWidget()])),
+          const Announcements(amount: 3)
+        ],
+      ),
+    );
   }
 }
