1b0e23bab815a9baef4d63f7beda5118503b0131
KristieW <kristiewesdorp@icloud.com>
2022-02-06 23:09:13 +0100

    First version of almanak profile page working.

--- lib/pages/almanak_profile.dart
+++ lib/pages/almanak_profile.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:graphql/client.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ksrvnjord_main_app/providers/heimdall.dart';
+import 'package:ksrvnjord_main_app/widgets/me/static_user_field.dart';
+import 'package:ksrvnjord_main_app/widgets/me/user_avatar.dart';
 
 const String users = r'''
   query {
@@ -22,7 +24,7 @@ class AlmanakProfile extends HookConsumerWidget {
     required this.profileId,
   }) : super(key: key);
 
-  final String profileId;
+  final int profileId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -33,28 +35,37 @@ class AlmanakProfile extends HookConsumerWidget {
     );
     final Future<QueryResult> result = client.query(options);
 
-  return FutureBuilder(
-      future: result,
-      builder: (BuildContext context, AsyncSnapshot<QueryResult> snapshot) {
-      switch (snapshot.connectionState) {
-        case ConnectionState.none:
-          return const Text('not started');
-        case ConnectionState.waiting:
-          return const Text('loading');
-        default:
-          var user = snapshot.data?.data?['users']['data'];
-          print(user);
-
-          return Scaffold(
-              appBar: AppBar(
-              title: const Text('Profile'),
-              ),
-              body: Card(
-                child: Text('Hello'),
-              ),
-          );
+    return FutureBuilder(
+        future: result,
+        builder: (BuildContext context, AsyncSnapshot<QueryResult> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Text('not started');
+            case ConnectionState.waiting:
+              return const Text('loading');
+            default:
+              var ind = snapshot.data?.data?['users']['data'].indexWhere((element) => element['identifier']==profileId);
+              var user = snapshot.data?.data?['users']['data'][ind];
+              // TODO: Dit efficienter maken
+              print(user['identifier'].runtimeType);
+              return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Profile'),
+                ),
+                body: ListView(
+                    children: <Widget>[
+                      const Center(child: UserAvatar()),
+                      const SizedBox(height: 10),
+                      const SizedBox(height: 20),
+                      StaticUserField('Naam', user['name'] ?? '-'),
+                      StaticUserField('Lidnummer', user['identifier'].toString() ?? '-'),
+                      StaticUserField('E-mailadres', user['email'] ?? '-'),
+                    //  StaticUserField('Telefoonnummer', user['phone_sms'] ?? '-'),
+                      StaticUserField('Njord-account', user['username'] ?? '-'),
+                    ]),
+              );
+          }
         }
-      },
     );
   }
 }
