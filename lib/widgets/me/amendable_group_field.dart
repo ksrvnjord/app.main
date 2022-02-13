49612820fdb3ae368c9961d5f79790232efd733c
PimVeefkind <71269094+PimVeefkind@users.noreply.github.com>
2022-02-14 00:23:22 +0100

    intermediate

--- lib/widgets/me/amendable_group_field.dart
+++ lib/widgets/me/amendable_group_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/widgets/me/group_grid.dart';
-import 'package:ksrvnjord_main_app/widgets/me/change_group_dialog.dart';
-import 'package:ksrvnjord_main_app/widgets/me/verification_dialog.dart';
+import 'package:ksrvnjord_main_app/widgets/me/group_icon.dart';
 
 class AmendableGroupField extends StatelessWidget {
   const AmendableGroupField(this.label, this.values, {Key? key})
@@ -27,29 +26,13 @@ class AmendableGroupField extends StatelessWidget {
           ],
         ),
         const Spacer(),
-        IconButton(
-          padding: const EdgeInsets.all(0),
-          constraints: const BoxConstraints(),
-          iconSize: 20,
-          icon: const Icon(Icons.edit, color: Colors.grey),
-          onPressed: () async {
-            bool succesful_change = await showDialog(
-                context: context,
-                builder: (BuildContext context) =>
-                    ChangeGroupinfoDialog(label: label));
-            if (succesful_change == true) {
-              String title = '''Verandering $label was succesvol!\n\n''';
-              const String body =
-                  '''Aangezien elke verandering door het bestuur moet worden goedgekeurd, kan het even duren voordat de verandering daadwerkelijk zichtbaar is.''';
-              showDialog(
-                  barrierDismissible: false,
-                  barrierColor: null,
-                  context: context,
-                  builder: (BuildContext context) =>
-                      VerificationDialog(title, body));
-            }
-          },
-        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GroupIcon(label, values, false),
+            GroupIcon(label, values, true)
+          ],
+        )
       ]),
       const Divider(
         color: Colors.grey,
