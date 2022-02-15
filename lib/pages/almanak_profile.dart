f7c9b51d88cee8799e4ed0e3d5c078de98b10411
Max Boone <contact@maxboone.com>
2022-02-15 23:25:19 +0100

    Use user-specific querying

--- lib/pages/almanak_profile.dart
+++ lib/pages/almanak_profile.dart
@@ -4,15 +4,17 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ksrvnjord_main_app/providers/heimdall.dart';
 import 'package:ksrvnjord_main_app/widgets/me/static_user_field.dart';
 import 'package:ksrvnjord_main_app/widgets/me/user_avatar.dart';
+import 'package:ksrvnjord_main_app/widgets/ui/general/loading.dart';
 
-const String users = r'''
-  query {
-    users {
-      data {
-        identifier,
-        name,
-        email,
-        username
+const String user = r'''
+  query ($identifier: Int) {
+    user (identifier: $identifier) {
+      identifier,
+      email,
+      username,
+      contact {
+        first_name,
+        last_name
       }
     }
   }
@@ -29,8 +31,8 @@ class AlmanakProfile extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final GraphQLClient client = ref.watch(heimdallProvider).graphQLClient();
-    final QueryOptions options = QueryOptions(
-        document: gql(users), variables: {'identifier': profileId});
+    final QueryOptions options =
+        QueryOptions(document: gql(user), variables: {'identifier': profileId});
     final Future<QueryResult> result = client.query(options);
 
     return FutureBuilder(
@@ -38,15 +40,12 @@ class AlmanakProfile extends HookConsumerWidget {
         builder: (BuildContext context, AsyncSnapshot<QueryResult> snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
-              return const Text('not started');
+              return const Loading();
             case ConnectionState.waiting:
-              return const Text('loading');
+              return const Loading();
             default:
-              var ind = snapshot.data?.data?['users']['data']
-                  .indexWhere((element) => element['identifier'] == profileId);
-              var user = snapshot.data?.data?['users']['data'][ind];
-              // TODO: Dit efficienter maken
-              print(user['identifier'].runtimeType);
+              var user = snapshot.data?.data?['user'];
+
               return Scaffold(
                 appBar: AppBar(
                   title: const Text('Profile'),
@@ -59,7 +58,6 @@ class AlmanakProfile extends HookConsumerWidget {
                   StaticUserField(
                       'Lidnummer', (user['identifier'] ?? '-').toString()),
                   StaticUserField('E-mailadres', user['email'] ?? '-'),
-                  //  StaticUserField('Telefoonnummer', user['phone_sms'] ?? '-'),
                   StaticUserField('Njord-account', user['username'] ?? '-'),
                 ]),
               );
