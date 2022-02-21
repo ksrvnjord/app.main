1469f75dadd2a7609e838009a5ce65bc8d9ad25e
N.J.L. Boonstra <nielsboonstra@caiway.net>
2022-02-21 17:57:51 +0100

    #31 Eerste implementatie met Markdown Editor en preview. Preview moet nog scrollable gemaakt worden. Verder moet er nog een verzendknop en verzendactie geimplementeerd worden.

--- lib/pages/main/announcements/add_announcement.dart
+++ lib/pages/main/announcements/add_announcement.dart
@@ -1,19 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:markdown_editable_textinput/markdown_text_input.dart';
 
-class AddAnnouncementPage extends HookConsumerWidget {
+class AddAnnouncementPage extends StatefulHookConsumerWidget {
   const AddAnnouncementPage({
     Key? key,
   }) : super(key: key);
 
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  AddAnnouncementForm createState() => AddAnnouncementForm();
+}
+
+class AddAnnouncementForm extends ConsumerState<AddAnnouncementPage> {
+  final _formKey = GlobalKey<FormState>(debugLabel: 'AddAnnouncementForm');
+  final _title = TextEditingController();
+  final _value = TextEditingController();
+
+  final EdgeInsets elemPadding = const EdgeInsets.fromLTRB(5, 10, 5, 10);
+
+  String _preview = "";
+
+  void showMarkDownPreview()
+  {
+    setState(() {
+      _preview = _value.text;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _value.addListener(showMarkDownPreview);
+  }
+
+  // @override
+  // void dispose() {
+  //   // Get rid of the resources we allocated
+  //   _title.dispose();
+  //   _value.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: const Text('Nieuwe Aankondiging'),
           backgroundColor: Colors.lightBlue,
           shadowColor: Colors.transparent,
         ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Container(
+                  padding: elemPadding,
+                  child: TextFormField(
+                    controller: _title,
+                    autocorrect: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Aankondiging titel',
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: elemPadding,
+                  child: MarkdownTextInput(
+                    // We do not use the onChanged method (which is required),
+                    // but the controller to listen for changes.
+                    (String value) {},
+                    '',
+                    controller: _value,
+                    label: 'Inhoud als Markdown',
+                  ),
+                ),
+                Container(
+                  padding: elemPadding,
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Voorbeeld van de inhoud van de aankondiging:"),
+                ),
+                // TODO: make scrollable, now it just overflows if the content
+                //       is large enough.
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: elemPadding,
+                  child: Markdown(
+                    data: _preview,
+                    selectable: false,
+                    shrinkWrap: true,  
+                  )
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
+
