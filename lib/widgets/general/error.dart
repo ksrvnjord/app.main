e4a345946f11b82d4d35cbefb2c0d4c46c747052
N.J.L. Boonstra <nielsboonstra@caiway.net>
2022-03-03 11:43:37 +0100

    #35 updated lay-out, should now always be consistent

--- lib/widgets/general/error.dart
+++ lib/widgets/general/error.dart
@@ -10,55 +10,69 @@ class ErrorCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color errorColor = Colors.red.shade800;
     return GestureDetector(
-      onTap: () {
-        const AlertDialog ad = AlertDialog(
-          content: Text("Dingen vragen hiero")
-        );
+      // onTap: () {
+      //   const AlertDialog ad = AlertDialog(
+      //     content: Text("Dingen vragen hiero")
+      //   );
 
-        showDialog(context: context, builder: (BuildContext context) {
-          return ad;
-        });
-      },
-      child: Card(
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-              side: BorderSide(color: errorColor, width: 2),
-              borderRadius: BorderRadius.circular(10)),
+      //   showDialog(context: context, builder: (BuildContext context) {
+      //     return ad;
+      //   });
+      // },s
+      child: Container(
+        height: 100.0,
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        decoration: BoxDecoration(
+          border: Border.all(width: 2.0, color: errorColor),
+          borderRadius: BorderRadius.circular(10)
+        ),
+
+        // semanticContainer: false,
+        // elevation: 0,
+        // shape: RoundedRectangleBorder(
+        //       side: BorderSide(color: errorColor, width: 2),
+        //       borderRadius: BorderRadius.circular(10)),
         clipBehavior: Clip.antiAlias,
         child: Row(
-          // crossAxisAlignment: CrossAxisAlignment.stretch, // This crashes
           children: [
             Expanded(
-              flex: 3,  // Use 30% of available width
-              child: Padding(
-                padding: const EdgeInsets.all(0),
-                child: Image.asset(Images.deadSwan),
-                // ClipRRect(
-                //   borderRadius: const BorderRadius.all(Radius.circular(5)),
-                //   child: Image.asset(Images.deadSwan),
-                // ),
-              )
+              flex: 0,
+              child: Image.asset(Images.deadSwan),
             ),
             Expanded(
-              flex: 7,
-              child: Column(
-                children: [
-                  Text(
-                    "Er is iets misgegaan",
-                    style: TextStyle(color: errorColor, fontSize: 18),
-                  ),
-                  Text(
-                    "Melding: " + errorMessage,
-                    style: const TextStyle(fontSize: 14),
-                  ),
-                  const Text(
-                    "Verzend een foutrapport door op deez te klikken",
-                    style: TextStyle(fontSize: 10),
-                  )
-                ],
-              )
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Er is iets misgegaan",
+                      style: TextStyle(color: errorColor, fontSize: 18),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          "Melding: " + errorMessage,
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                        alignment: Alignment.center,
+                      ) 
+                    ),
+                    // const Expanded(
+                    //   flex: 1,
+                    //   child: Text(
+                    //     "Tik hier om een foutrapport te verzenden",
+                    //     style: TextStyle(fontSize: 10),
+                    //   )
+                    // )
+                  ]
+                )
+              ) 
             )
-          ],
+            
+          ]
         )
       )
     );
