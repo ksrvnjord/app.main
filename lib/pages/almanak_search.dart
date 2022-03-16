e226e90c01c89ffe0c7a4fd4cf44eb369a117c02
KristieW <kristiewesdorp@icloud.com>
2022-03-16 15:02:04 +0100

    Update almanak_search.dart

--- lib/pages/almanak_search.dart
+++ lib/pages/almanak_search.dart
@@ -10,22 +10,38 @@ class AlmanakSearch extends StatefulWidget {
 }
 
 class _AlmanakSearchState extends State<AlmanakSearch> {
-  String currentSearch = 'kristie';
+  String currentSearch = '';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextField(
-          autofocus: true,
-          onChanged: (text) {
-            currentSearch = text;
-          },
-        ),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.lightBlue,
         shadowColor: Colors.transparent,
+        title: Row(
+          children: [
+            TextField(
+              autofocus: true,
+              onChanged: (text) {
+                setState(() {
+                  currentSearch = text;
+                  });
+                },
+              ),
+            IconButton(
+              onPressed: () {
+                currentSearch = '';
+              },
+              icon: const Icon(Icons.clear),
+              )],),
       ),
       body: Text(currentSearch),
   );
 }
+  @override
+  List<Widget> clearSearch(BuildContext context) {
+    return [
+
+    ];
+  }
 }
