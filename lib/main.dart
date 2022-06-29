9f0b8ffceda1c3c68274af8346b64758db961ed9
KristieW <kristiewesdorp@icloud.com>
2022-06-29 12:41:53 +0200

    Update 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ksrvnjord_main_app/providers/authentication.dart';
 import 'package:ksrvnjord_main_app/screens/main.dart';
 import 'package:ksrvnjord_main_app/screens/login.dart';
-import 'package:ksrvnjord_main_app/widgets/ui/general/loading.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 
@@ -29,8 +28,8 @@ Future<void> main() async {
 }
 
 class MyApp extends HookConsumerWidget {
-  MyApp({Key? key}) : super(key: key);
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -38,18 +37,7 @@ class MyApp extends HookConsumerWidget {
 
     return MaterialApp(
       title: 'K.S.R.V. Njord',
-      home: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            const Loading();
-          } else if (snapshot.hasData) {
-          authenticator.loggedIn ? const MainScreen() : const LoginScreen();
-          } else {
-            const Loading();
-          }
-        },
-      ),
+      home: authenticator.loggedIn ? const MainScreen() : const LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
