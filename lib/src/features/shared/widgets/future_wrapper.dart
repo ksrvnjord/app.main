2ca2e4128f989604eb3267c089d840198a326bb3
Max Boone <contact@maxboone.com>
2022-09-07 23:18:52 +0200

    Rename widget folder and make FutureWrapper more resilient

---
+++ lib/src/features/shared/widgets/future_wrapper.dart
@@ -0,0 +1,44 @@
+import 'package:styled_widget/styled_widget.dart';
+import 'package:flutter/material.dart';
+
+Widget? onEmpty<T>(T arg) {
+  return Text(arg.toString());
+}
+
+class FutureWrapper<T> extends StatelessWidget {
+  final Future<T> future;
+  final Widget? loading;
+  final Widget? Function(Object? error)? error;
+  final Widget? Function(dynamic data)? success;
+
+  const FutureWrapper(
+      {Key? key,
+      required this.future,
+      this.loading,
+      this.error = onEmpty,
+      this.success = onEmpty})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<T>(
+      future: future,
+      builder: (context, AsyncSnapshot<T> snapshot) {
+        if (snapshot.hasData) {
+          return success != null
+              ? success!(snapshot.data) ?? Container()
+              : Container();
+        }
+
+        if (snapshot.hasError) {
+          return error != null
+              ? error!(snapshot.data) ?? Container()
+              : Container();
+        }
+
+        return loading ??
+            const CircularProgressIndicator().padding(all: 10).expanded();
+      },
+    );
+  }
+}
