9ad76f44e6cf0a2342165058865f09dc39e0961e
Max Boone <contact@maxboone.com>
2022-09-11 16:02:16 +0200

    Remove obsoleted URLs

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,8 +5,6 @@ class Endpoint {
   static const String oauthId = '11';
   static const String oauthSecret = 'YvbiNaGmgkBBNPbJ5REwtviiPxMb1tCXrdqIhqNQ';
   static final Uri oauthEndpoint = Uri.parse('$baseURL/oauth/token');
-  static const String csrf = 'api/v1/auth/csrf';
-  static const String login = 'api/v1/auth/login';
   static const String user = 'api/v1/user';
   static const String vaarverbod = 'api/v1/vaarverbod';
   static const String graphql = 'graphql';
