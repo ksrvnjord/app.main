0ae1266cdffe4a0e4ad484a32e532f94dc061ced
Max Boone <contact@maxboone.com>
2022-09-11 16:39:40 +0200

    Add shimmer effect

--- lib/src/features/dashboard/widgets/vaarverbod_widget.dart
+++ lib/src/features/dashboard/widgets/vaarverbod_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class VaarverbodWidget extends StatelessWidget {
@@ -21,9 +22,8 @@ class VaarverbodWidget extends StatelessWidget {
 
             return <Widget>[
               r['status']
-                  ? Icon(Icons.priority_high, color: fgColor, size: 15)
-                      .padding(all: 10)
-                  : Icon(Icons.favorite_outlined, color: fgColor, size: 15)
+                  ? Icon(Icons.priority_high, color: fgColor).padding(all: 10)
+                  : Icon(Icons.favorite_outlined, color: fgColor)
                       .padding(all: 10),
               Text(r['message'],
                       textAlign: TextAlign.right,
@@ -40,6 +40,24 @@ class VaarverbodWidget extends StatelessWidget {
           }
 
           return Container();
-        });
+        },
+        loading: Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: <Widget>[
+              const Icon(Icons.downloading, color: Colors.white)
+                  .padding(all: 10),
+              const Text('Laden...',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 15,
+                          color: Colors.white))
+                  .padding(all: 10)
+                  .expanded()
+            ]
+                .toRow()
+                .card(color: Colors.grey, elevation: 0)
+                .padding(all: 15, bottom: 0)));
   }
 }
