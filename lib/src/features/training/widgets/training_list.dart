132f778935ae557fd04f92942b53c5088758ce08
PimVeefkind <pim.veefkind@gmail.com>
2022-10-29 21:42:44 +0200

    included reference call

--- lib/src/features/training/widgets/training_list.dart
+++ lib/src/features/training/widgets/training_list.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:lucide_icons/lucide_icons.dart';
-import 'package:styled_widget/styled_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/training/model/reservation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ksrvnjord_main_app/src/features/training/model/reservationObject.dart';
 
 class TrainingList extends StatelessWidget {
   const TrainingList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final CollectionReference reservations =
-        FirebaseFirestore.instance.collection('reservations');
-    final CollectionReference reservationObjects =
-        FirebaseFirestore.instance.collection('reservationObjects');
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    final CollectionReference<Reservation> reservationsRef =
+        db.collection('reservations').withConverter<Reservation>(
+              fromFirestore: (snapshot, _) =>
+                  Reservation.fromJson(snapshot.data()!),
+              toFirestore: (reservation, _) => reservation.toJson(),
+            );
+    final CollectionReference reservationObjectsRef =
+        db.collection('reservationObjects');
     DateFormat dateFormat = DateFormat('MMM d, hh:mm');
 
     String trainingTimeFromTimestamps(reservation) {
-      final DateTime startTime = reservation?.get('startTime').toDate();
-      final DateTime endTime = reservation?.get('endTime').toDate();
+      final DateTime startTime = reservation['startTime'].toDate();
+      final DateTime endTime = reservation['endTime'].toDate();
 
       final String stringStart = dateFormat.format(startTime);
       final String stringEnd = DateFormat.Hm().format(endTime);
@@ -25,12 +30,13 @@ class TrainingList extends StatelessWidget {
       return ('$stringStart-$stringEnd');
     }
 
-    return FutureBuilder<QuerySnapshot>(
-        future: reservations
+    return FutureBuilder<QuerySnapshot<Reservation>>(
+        future: reservationsRef
             .where('creatorId', isEqualTo: 21203)
             .orderBy('startTime', descending: true)
             .get(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Reservation>> snapshot) {
           if (snapshot.hasError) {
             return const Text(
                 'Laden ingeplande trainingen niet mogelijk. Probeer het later opnieuw');
@@ -43,24 +49,46 @@ class TrainingList extends StatelessWidget {
               child: CircularProgressIndicator(),
             ));
           } else {
-            return snapshot.data?.docs != null
-                ? ListView.separated(
-                    itemCount: snapshot.data?.docs.length ?? 0,
-                    padding: const EdgeInsets.all(10),
-                    separatorBuilder: (context, index) => const SizedBox(
-                          height: 10,
-                        ),
-                    itemBuilder: (context, index) {
-                      QueryDocumentSnapshot<Object?>? reservation =
-                          snapshot.data?.docs[index];
-                      return ListTile(
-                        tileColor: Colors.white,
-                        title: Text(reservation?.get('object_name')),
-                        subtitle: Text(trainingTimeFromTimestamps(reservation)),
-                        leading: const Icon(Icons.fitness_center),
-                      );
-                    })
-                : Container();
+            return ListView.separated(
+                itemCount: snapshot.data?.docs.length ?? 0,
+                padding: const EdgeInsets.all(10),
+                separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    ),
+                itemBuilder: (context, index) {
+                  QueryDocumentSnapshot<Reservation> reservation =
+                      snapshot.data!.docs[index];
+                  return FutureBuilder<DocumentSnapshot>(
+                      future: reservationObjectsRef
+                          .doc(reservation['object'].id)
+                          .get(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (snapshot.hasError) {
+                          return const Text(
+                              'Laden ingeplande training niet mogelijk');
+                        }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: SizedBox(
+                            height: 50,
+                            width: 50,
+                            child: CircularProgressIndicator(),
+                          ));
+                        } else {
+                          Map<String, dynamic> boat =
+                              snapshot.data!.data() as Map<String, dynamic>;
+                          return ListTile(
+                            tileColor: Colors.white,
+                            title: Text(boat['name']),
+                            subtitle:
+                                Text(trainingTimeFromTimestamps(reservation)),
+                            leading: const Icon(Icons.fitness_center),
+                          );
+                        }
+                      });
+                });
           }
         });
   }
