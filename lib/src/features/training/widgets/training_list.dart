a8c1dc801702126c3660c533c6b81b67a3aa947b
PimVeefkind <pim.veefkind@gmail.com>
2022-11-05 19:19:24 +0100

    reservering annuleren

--- lib/src/features/training/widgets/training_list.dart
+++ lib/src/features/training/widgets/training_list.dart
@@ -1,95 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ksrvnjord_main_app/src/features/training/model/reservationObject.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/training_list_item.dart';
 
-class TrainingList extends StatelessWidget {
-  const TrainingList({Key? key}) : super(key: key);
+class TrainingList extends StatefulWidget {
+  const TrainingList({super.key});
+
+  @override
+  State<TrainingList> createState() => TrainingListState();
+}
+
+class TrainingListState extends State<TrainingList> {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
-    FirebaseFirestore db = FirebaseFirestore.instance;
     final CollectionReference<Reservation> reservationsRef =
         db.collection('reservations').withConverter<Reservation>(
               fromFirestore: (snapshot, _) =>
                   Reservation.fromJson(snapshot.data()!),
               toFirestore: (reservation, _) => reservation.toJson(),
             );
-    final CollectionReference reservationObjectsRef =
-        db.collection('reservationObjects');
-    DateFormat dateFormat = DateFormat('MMM d, hh:mm');
 
-    String trainingTimeFromTimestamps(reservation) {
-      final DateTime startTime = reservation['startTime'].toDate();
-      final DateTime endTime = reservation['endTime'].toDate();
-
-      final String stringStart = dateFormat.format(startTime);
-      final String stringEnd = DateFormat.Hm().format(endTime);
-
-      return ('$stringStart-$stringEnd');
-    }
-
-    return FutureBuilder<QuerySnapshot<Reservation>>(
-        future: reservationsRef
+    return StreamBuilder(
+        stream: reservationsRef
             .where('creatorId', isEqualTo: 21203)
-            .orderBy('startTime', descending: true)
-            .get(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Reservation>> snapshot) {
-          if (snapshot.hasError) {
-            return const Text(
-                'Laden ingeplande trainingen niet mogelijk. Probeer het later opnieuw');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
+            .where('startTime',
+                isGreaterThanOrEqualTo:
+                    DateTime.now().subtract(const Duration(days: 1)))
+            .orderBy('startTime', descending: false)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
             return const Center(
-                child: SizedBox(
-              height: 50,
-              width: 50,
               child: CircularProgressIndicator(),
-            ));
-          } else {
-            return ListView.separated(
-                itemCount: snapshot.data?.docs.length ?? 0,
-                padding: const EdgeInsets.all(10),
-                separatorBuilder: (context, index) => const SizedBox(
-                      height: 10,
-                    ),
-                itemBuilder: (context, index) {
-                  QueryDocumentSnapshot<Reservation> reservation =
-                      snapshot.data!.docs[index];
-                  return FutureBuilder<DocumentSnapshot>(
-                      future: reservationObjectsRef
-                          .doc(reservation['object'].id)
-                          .get(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<DocumentSnapshot> snapshot) {
-                        if (snapshot.hasError) {
-                          return const Text(
-                              'Laden ingeplande training niet mogelijk');
-                        }
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: SizedBox(
-                            height: 50,
-                            width: 50,
-                            child: CircularProgressIndicator(),
-                          ));
-                        } else {
-                          Map<String, dynamic> boat =
-                              snapshot.data!.data() as Map<String, dynamic>;
-                          return ListTile(
-                            tileColor: Colors.white,
-                            title: Text(boat['name']),
-                            subtitle:
-                                Text(trainingTimeFromTimestamps(reservation)),
-                            leading: const Icon(Icons.fitness_center),
-                          );
-                        }
-                      });
-                });
+            );
           }
+          return ListView.separated(
+              itemCount: snapshot.data!.docs.length,
+              padding: const EdgeInsets.all(10),
+              separatorBuilder: (BuildContext context, int index) =>
+                  const SizedBox(height: 10),
+              itemBuilder: (BuildContext context, int index) {
+                QueryDocumentSnapshot<Object?> reservation =
+                    snapshot.data!.docs[index];
+                return Center(
+                    child: TrainingListItem(reservation: reservation));
+              });
         });
   }
 }
