a3ee6fa779c3a1e50828450457cc9206aa764958
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-11-23 20:29:43 +0100

    better error message when filters arent retrieved

--- lib/src/features/training/widgets/training_filters.dart
+++ lib/src/features/training/widgets/training_filters.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql/client.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/filters.dart';
 import 'package:provider/provider.dart';
@@ -38,8 +39,9 @@ class TrainingFilters extends StatelessWidget {
                   title: Text(e.description),
                   onTap: () => toggleFilter(e.type));
             }).toList());
+          } else {
+            return const ErrorCardWidget(errorMessage: 'Er is iets misgegaan met het ophalen van de filters');
           }
-          return Container();
         });
   }
 }
