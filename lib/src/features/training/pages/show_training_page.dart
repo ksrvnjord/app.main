4fe2c0684c305697d6131f0770903268cdb39d33
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-16 21:04:30 +0100

    show reservation details

--- lib/src/features/training/pages/show_training_page.dart
+++ lib/src/features/training/pages/show_training_page.dart
@@ -1,10 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error.dart';
-import 'package:provider/provider.dart';
 
 class ShowTrainingPage extends StatelessWidget {
   final String id;
@@ -15,7 +12,6 @@ class ShowTrainingPage extends StatelessWidget {
     CollectionReference users = FirebaseFirestore.instance.collection('people');
     CollectionReference reservations =
         FirebaseFirestore.instance.collection('reservations');
-    print("document id: $id");
     return Scaffold(
         appBar: AppBar(
           title: const Text('Afschrijving'),
@@ -26,38 +22,70 @@ class ShowTrainingPage extends StatelessWidget {
               const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
         ),
         body: FutureBuilder<DocumentSnapshot>(
-          future: reservations.doc(id).get(),
-          builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const ErrorCardWidget(
-                errorMessage: "We konden geen verbinding maken met de database");
-          }
-          if (snapshot.hasData && !snapshot.data!.exists) {
-            return const ErrorCardWidget(
-                errorMessage: "Er is geen afschrijving gevonden");
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> data =
-                snapshot.data!.data() as Map<String, dynamic>;
-            return Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: TextFormField(
-                    enabled: false,
-                    decoration: const InputDecoration(
-                      labelText: 'Afschrijver',
-                      border: OutlineInputBorder(),
-                    ),
-                    initialValue: data['creatorId'],
-                    style: const TextStyle(color: Colors.black54),
-                  ),
-                ),
-              ],
-            );
-          }
-          return const CircularProgressIndicator();
-        }));
+            future: reservations.doc(id).get(),
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const ErrorCardWidget(
+                    errorMessage:
+                        "We konden geen verbinding maken met de database");
+              }
+              if (snapshot.hasData && !snapshot.data!.exists) {
+                return const ErrorCardWidget(
+                    errorMessage: "Er is geen afschrijving gevonden");
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> reservation =
+                    snapshot.data!.data() as Map<String, dynamic>;
+
+                return FutureBuilder<DocumentSnapshot>(
+                    future: users.doc(reservation['creatorId']).get(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<DocumentSnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return const ErrorCardWidget(
+                            errorMessage:
+                                "We konden de afschrijver niet ophalen");
+                      }
+                      if (snapshot.hasData && !snapshot.data!.exists) {
+                        return const ErrorCardWidget(
+                            errorMessage:
+                                "We konden de afschrijver niet ophalen");
+                      }
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        Map<String, dynamic> user =
+                            snapshot.data!.data() as Map<String, dynamic>;
+                        return ListView(children: [
+                          ListTile(
+                            leading: const Icon(Icons.person),
+                            title: Text(
+                                user['first_name'] + ' ' + user['last_name']),
+                          ),
+                          Divider(),
+                          ListTile(
+                            leading: const Icon(Icons.start),
+                            title: Text(timestampToTimeOfDay(
+                                reservation['startTime']!, context)),
+                          ),
+                          Divider(),
+                          ListTile(
+                            leading: const Icon(Icons.stop),
+                            title: Text(timestampToTimeOfDay(
+                                reservation['endTime']!, context)),
+                          ),
+                        ]);
+                      }
+                      return const CircularProgressIndicator();
+                    });
+              }
+              return const CircularProgressIndicator();
+            }));
   }
+}
+
+String timestampToTimeOfDay(Timestamp timestamp, BuildContext context) {
+  DateTime date =
+      DateTime.fromMillisecondsSinceEpoch(timestamp.millisecondsSinceEpoch);
+  TimeOfDay time = TimeOfDay.fromDateTime(date);
+  return time.format(context);
 }
