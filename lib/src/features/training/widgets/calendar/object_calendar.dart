5140849bc8a8694225826651925c318a93af971b
Max Boone <max@netpunt.nl>
2022-12-17 18:46:55 +0100

    Further work on the calendar refactor

--- lib/src/features/training/widgets/calendar/object_calendar.dart
+++ lib/src/features/training/widgets/calendar/object_calendar.dart
@@ -4,8 +4,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservationObject.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/slots.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/atoms/calendar_background.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/atoms/calendar_reservation.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -24,8 +27,17 @@ class ObjectCalendar extends StatefulWidget {
 }
 
 class _ObjectCalendar extends State<ObjectCalendar> {
+  // Date is passed by parent
   late DateTime date;
+  // Boat is passed by parent
   late QueryDocumentSnapshot<ReservationObject> boat;
+
+  // Create reference for query
+  CollectionReference reservationRef =
+      FirebaseFirestore.instance.collection('reservations');
+
+  // Get current time
+  DateTime now = DateTime.now();
 
   @override
   void initState() {
@@ -36,164 +48,29 @@ class _ObjectCalendar extends State<ObjectCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    final DateTime earliestDateTimeThatCanBeBooked =
-        DateTime(date.year, date.month, date.day, 6, 0); // 6:00
-    final DateTime latestDateTimeThatCanBeBooked =
-        DateTime(date.year, date.month, date.day, 22, 0); // 22:00
-    final DateTimeRange dateTimeRange = DateTimeRange(
-        start: earliestDateTimeThatCanBeBooked,
-        end: latestDateTimeThatCanBeBooked);
-    final int minutes = dateTimeRange.duration.inMinutes;
-    const int timeSlotSize = 30; // minutes
+    DateTime nowStart = DateTime(now.year, now.month, now.day, 6, 0, 0);
+    DateTime nowEnd = DateTime(now.year, now.month, now.day, 22, 0, 0);
 
-    final int amountOfSlots = minutes ~/ timeSlotSize;
-
-    final List<DateTime> timestamps = List.generate(
-        amountOfSlots,
-        (i) => earliestDateTimeThatCanBeBooked
-            .add(Duration(minutes: i * timeSlotSize)));
-
-    var navigator = Routemaster.of(context);
-    CollectionReference reservationRef =
-        FirebaseFirestore.instance.collection('reservations');
-    DateTime now = DateTime.now();
-
-    List<Map<String, dynamic>> reservationsToReservedSlots(
-        List<QueryDocumentSnapshot> reservations) {
-      List<Map<String, dynamic>> forbiddenSlots = [];
-      Duration interval = const Duration(minutes: timeSlotSize);
-      for (int i = 0; i < reservations.length; i++) {
-        DateTime startTime = reservations[i].get('startTime').toDate();
-        DateTime endTime = reservations[i].get('endTime').toDate();
-        DateTime roundedStart = DateTime(
-            startTime.year,
-            startTime.month,
-            startTime.day,
-            startTime.hour,
-            [0, timeSlotSize][(startTime.minute / timeSlotSize).floor()]);
-        DateTime roundedEnd = DateTime(
-            endTime.year,
-            endTime.month,
-            endTime.day,
-            endTime.hour,
-            [0, timeSlotSize][(endTime.minute / timeSlotSize).floor()]);
-        DateTime current = roundedStart;
-        while (current.isBefore(roundedEnd)) {
-          forbiddenSlots
-              .add({'time': current, 'reservationId': reservations[i].id});
-          current = current.add(interval);
-        }
-      }
-      return forbiddenSlots;
-    }
+    // Get all reservations
+    final _reservations = reservationRef
+        .where('object', isEqualTo: boat.reference)
+        .where('startTime', isGreaterThanOrEqualTo: nowStart)
+        .where('startTime', isLessThanOrEqualTo: nowEnd)
+        .get(const GetOptions(source: Source.serverAndCache));
 
     return SizedBox(
         width: 96,
-        child: <Widget>[
-          FutureBuilder(
-              future: reservationRef
-                  .where('object', isEqualTo: boat.reference)
-                  .where('startTime',
-                      isGreaterThanOrEqualTo: earliestDateTimeThatCanBeBooked)
-                  .where('startTime',
-                      isLessThanOrEqualTo: latestDateTimeThatCanBeBooked)
-                  .get(const GetOptions(source: Source.serverAndCache)),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return const ErrorCardWidget(
-                      errorMessage:
-                          "Er is iets misgegaan bij het ophalen van de afschrijvingen");
-                }
-
-                List<Map<String, dynamic>> forbiddenSlots = [];
-                if (snapshot.data?.docs != null) {
-                  forbiddenSlots =
-                      reservationsToReservedSlots(snapshot.data!.docs);
-                }
-                return Stack(children: <Widget>[
-                  SizedBox(
-                      width: 96,
-                      child: timestamps
-                          .map<Widget>((timestamp) => SizedBox(
-                                  height: 32,
-                                  width: 96,
-                                  child: () {
-                                    // check if forbiddenslots contains timestamp
-                                    bool reserved = false;
-                                    for (Map<String, dynamic> forbiddenSlot
-                                        in forbiddenSlots) {
-                                      if (forbiddenSlot['time'] == timestamp) {
-                                        reserved = true;
-                                        return GestureDetector(
-                                            onTap: () {
-                                              navigator.push(forbiddenSlot[
-                                                  'reservationId']);
-                                            },
-                                            child:
-                                                Container(color: Colors.grey));
-                                      }
-                                    }
-                                    if (reserved) {
-                                      // ignore: empty_statements
-                                      ;
-                                    } else if (FirebaseAuth
-                                            .instance.currentUser ==
-                                        null) {
-                                      return Container(
-                                          color: const Color.fromARGB(
-                                              255, 245, 245, 245));
-                                    } else {
-                                      return IconButton(
-                                          icon: const Icon(
-                                              LucideIcons.plusCircle,
-                                              size: 12,
-                                              color: Colors.grey),
-                                          onPressed: () {
-                                            navigator
-                                                .push('plan', queryParameters: {
-                                                  'reservationObjectId':
-                                                      boat.id,
-                                                  'reservationObjectName':
-                                                      boat.get('name'),
-                                                  'startTime': timestamp
-                                                      .toIso8601String(),
-                                                })
-                                                .result
-                                                .then((success) {
-                                                  if (success == true) {
-                                                    setState(() =>
-                                                        {}); // refresh page
-                                                  }
-                                                });
-                                          });
-                                    }
-                                  }())
-                              .border(
-                                  bottom: 1,
-                                  color:
-                                      const Color.fromARGB(255, 245, 245, 245)))
-                          .toList()
-                          .toColumn(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center)),
-                  ...[].map<Widget>((e) {
-                    return Positioned(
-                        top: ((((e.from.hour - 6) * 60) + (e.from.minute)) /
-                                30) *
-                            32,
-                        child: Container(
-                          width: 96,
-                          height: (e.length.inMinutes / 30) * 32,
-                          decoration: BoxDecoration(
-                              color: Colors.blueAccent,
-                              border:
-                                  Border.all(width: 4, color: Colors.white)),
-                        ));
-                  }).toList()
-                ]).border(
-                    right: 1, color: const Color.fromARGB(255, 225, 225, 225));
-              })
-        ].toColumn());
+        child: Stack(
+          children: [
+            const CalendarBackground(fragmentHeight: 32),
+            FutureWrapper(
+                future: _reservations,
+                success: (reservations) {
+                  if (reservations != null) {
+                    return Text(reservations.docs.toList().toString());
+                  }
+                })
+          ],
+        ));
   }
 }
