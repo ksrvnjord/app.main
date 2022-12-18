e5e393bd59dac8374e2b87564de2fdbc32198920
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-18 15:31:56 +0100

    afschrijfbutton to more sleek round button

--- lib/src/features/training/pages/training_page.dart
+++ lib/src/features/training/pages/training_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/training_home_nav.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/training_list.dart';
+import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class TrainingPage extends StatelessWidget {
@@ -9,6 +10,7 @@ class TrainingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Routemaster navigator = Routemaster.of(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -21,8 +23,13 @@ class TrainingPage extends StatelessWidget {
       ),
       body: <Widget>[
         const TrainingList().expanded(),
-        const TrainingHomeNav(),
+        // const TrainingHomeNav(),
       ].toColumn(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => navigator.push('all'),
+        backgroundColor: Colors.lightBlue,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
