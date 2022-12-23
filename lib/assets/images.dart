d76bb7410efc4f2f51dbffb667be6dc24fe03f50
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-23 22:02:03 +0100

    add file picker and placeholder image

--- lib/assets/images.dart
+++ lib/assets/images.dart
@@ -3,4 +3,5 @@ class Images {
 
   static const appLogo = "assets/images/njord_w.png";
   static const deadSwan = "assets/images/deadswan.png";
+  static const placeholderProfilePicture = "assets/images/placeholder_profile_picture.png";
 }
--- lib/src/features/profiles/pages/edit_almanak_profile_page.dart
+++ lib/src/features/profiles/pages/edit_almanak_profile_page.dart
@@ -1,5 +1,7 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ksrvnjord_main_app/assets/images.dart';
 
 class EditAlmanakProfilePage extends StatefulWidget {
   const EditAlmanakProfilePage({Key? key}) : super(key: key);
@@ -9,6 +11,8 @@ class EditAlmanakProfilePage extends StatefulWidget {
 }
 
 class _EditAlmanakProfilePageState extends State<EditAlmanakProfilePage> {
+  var _selectedFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +23,37 @@ class _EditAlmanakProfilePageState extends State<EditAlmanakProfilePage> {
         systemOverlayStyle:
             const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
       ),
-      body: const Center(
-        child: Text('Edit Almanak Profile'),
+      body: Center(
+        child: Column(
+      children: [
+        // Display the selected profile picture, or a placeholder if no picture has been selected
+        _selectedFile != null
+            ? Image.file(_selectedFile)
+            : Image.asset(Images.placeholderProfilePicture),
+        MaterialButton(
+          onPressed: () async {
+            // Open a file picker dialog
+            _selectedFile = await FilePicker.platform.pickFiles(
+              type: FileType.image,
+              allowMultiple: false
+            );
+            // Update the UI to show the selected profile picture
+            setState(() {});
+          },
+          color: Colors.lightBlue,
+          textColor: Colors.white,
+          child: const Text('Kies foto'),
+        ),
+      ],
+    ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Code to save the profile changes goes here
+        },
+        child: const Icon(Icons.save),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
