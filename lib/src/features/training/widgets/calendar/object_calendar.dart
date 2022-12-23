1f2a3f49f2ea5171029682d44231ac6d6452be8a
Max Boone <max@netpunt.nl>
2022-12-23 13:19:29 +0100

    Add shimmering and ID of reservation

--- lib/src/features/training/widgets/calendar/object_calendar.dart
+++ lib/src/features/training/widgets/calendar/object_calendar.dart
@@ -5,12 +5,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/shimmer_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservationObject.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/slots.dart';
-import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/atoms/calendar_background.dart';
-import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/atoms/calendar_reservation.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/widgets/calendar_background.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/widgets/calendar_reservation.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:routemaster/routemaster.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class ObjectCalendar extends StatefulWidget {
@@ -48,11 +50,12 @@ class _ObjectCalendar extends State<ObjectCalendar> {
 
   @override
   Widget build(BuildContext context) {
-    DateTime nowStart = DateTime(now.year, now.month, now.day, 6, 0, 0);
-    DateTime nowEnd = DateTime(now.year, now.month, now.day, 22, 0, 0);
+    // Calculate start and end of day
+    DateTime nowStart = DateTime(date.year, date.month, date.day, 6, 0, 0);
+    DateTime nowEnd = DateTime(date.year, date.month, date.day, 22, 0, 0);
 
-    // Get all reservations
-    final _reservations = reservationRef
+    // Get all reservations within that time period
+    final reservations = reservationRef
         .where('object', isEqualTo: boat.reference)
         .where('startTime', isGreaterThanOrEqualTo: nowStart)
         .where('startTime', isLessThanOrEqualTo: nowEnd)
@@ -60,15 +63,31 @@ class _ObjectCalendar extends State<ObjectCalendar> {
 
     return SizedBox(
         width: 96,
+        // Stack the elements over eachother
         child: Stack(
           children: [
+            // Horizontal bars of 32h
             const CalendarBackground(fragmentHeight: 32),
+            // Wrap the reservations
             FutureWrapper(
-                future: _reservations,
+                future: reservations,
+                // Shimmer entire screen on loading
+                loading: const ShimmerWidget(
+                    child: SizedBox(height: 32 * 32, width: 96)),
+                // Create a stack of the resulting reservations
                 success: (reservations) {
                   if (reservations != null) {
-                    return Text(reservations.docs.toList().toString());
+                    return reservations.docs
+                        .map((reservation) {
+                          return CalendarReservation(
+                              fragmentHeight: 32,
+                              data: reservation.data(),
+                              id: reservation.id);
+                        })
+                        .toList()
+                        .toStack();
                   }
+                  // Or just return an empty container if it fails
                   return Container();
                 })
           ],
