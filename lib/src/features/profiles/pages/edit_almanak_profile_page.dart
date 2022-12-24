f49d546cbd29c2d920c2dd21abed713591519172
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-24 10:58:42 +0100

    add toast on successful upload

--- lib/src/features/profiles/pages/edit_almanak_profile_page.dart
+++ lib/src/features/profiles/pages/edit_almanak_profile_page.dart
@@ -1,10 +1,14 @@
+import 'dart:developer';
 import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker_widget/image_picker_widget.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+final storage = FirebaseStorage.instance;
 
 class EditAlmanakProfilePage extends StatefulWidget {
   const EditAlmanakProfilePage({Key? key}) : super(key: key);
@@ -14,10 +18,13 @@ class EditAlmanakProfilePage extends StatefulWidget {
 }
 
 class _EditAlmanakProfilePageState extends State<EditAlmanakProfilePage> {
-  var _selectedFile;
+  final String userId = FirebaseAuth.instance.currentUser!.uid;
 
   @override
   Widget build(BuildContext context) {
+    final Reference userRef = storage.ref().child(userId);
+    final Reference profilePictureRef = userRef.child('profile_picture.png');
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wijzig mijn almanak profiel'),
@@ -32,16 +39,23 @@ class _EditAlmanakProfilePageState extends State<EditAlmanakProfilePage> {
           child: ImagePickerWidget(
             diameter: 240,
             initialImage: const AssetImage(Images.placeholderProfilePicture),
-            shape: ImagePickerWidgetShape
-                .circle, // ImagePickerWidgetShape.square
+            shape:
+                ImagePickerWidgetShape.circle, // ImagePickerWidgetShape.square
             isEditable: true,
-            onChange: (File file) {
-              print("I changed the file to: ${file.path}");
+            onChange: (File file) async {
+              try {
+                await profilePictureRef.putFile(file);
+                Fluttertoast.showToast(
+                    msg: "Your profile picture was updated successfully");
+              } on FirebaseException catch (e) {
+                Fluttertoast.showToast(
+                    msg: "Error updating your profile picture");
+                log(e.toString());
+              }
             },
           ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
