63530a588e6205bb16f9cd4ce2245886335d0c67
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-24 14:12:08 +0100

    show profile pictures at almanak list

--- lib/src/features/profiles/widgets/almanak_user_button_widget.dart
+++ lib/src/features/profiles/widgets/almanak_user_button_widget.dart
@@ -1,31 +1,71 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/almanak.graphql.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_picture.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/models/profile.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
+import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:styled_widget/styled_widget.dart';
 
 class AlmanakUserButtonWidget extends StatelessWidget {
   final Query$Almanak$users$data user;
 
   const AlmanakUserButtonWidget(this.user, {Key? key}) : super(key: key);
-@override
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Routemaster.of(context).push('/almanak/${user.id}');
-      },
-      child: Card(
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundImage: Image.asset(Images.placeholderProfilePicture).image,
-          ),
-          title: Text('${user.fullContact.public.first_name ?? ''} ${user.fullContact.public.last_name ?? ''}'),
-          onTap: () {
-            Routemaster.of(context).push('/almanak/${user.id}');
-          },
-        ),
+    final client = Provider.of<GraphQLModel>(context).client;
+
+    return Card(
+      child: ListTile(
+        leading: FutureBuilder(
+            future: almanakProfile(user.id, client),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return CircleAvatar(
+                  backgroundImage:
+                      Image.asset(Images.placeholderProfilePicture).image,
+                );
+              } else if (snapshot.hasData) {
+                String userId = snapshot.data!.identifier;
+
+                return FutureBuilder(
+                    future: getProfilePicture(userId),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return CircleAvatar(
+                          backgroundImage:
+                              Image.asset(Images.placeholderProfilePicture)
+                                  .image,
+                        );
+                      } else if (snapshot.hasData) {
+                        MemoryImage image =
+                            MemoryImage(snapshot.data as Uint8List);
+
+                        return CircleAvatar(
+                          backgroundImage: image,
+                        );
+                      }
+
+                      return CircleAvatar(
+                        backgroundImage:
+                            Image.asset(Images.placeholderProfilePicture).image,
+                      );
+                    });
+              } else {
+                return CircleAvatar(
+                  backgroundImage:
+                      Image.asset(Images.placeholderProfilePicture).image,
+                );
+              }
+            }),
+        title: Text(
+            '${user.fullContact.public.first_name ?? ''} ${user.fullContact.public.last_name ?? ''}'),
+        onTap: () {
+          Routemaster.of(context).push('/almanak/${user.id}');
+        },
       ),
     );
   }
-
 }
