b07e0517ad957361167d2d864b51d9babdf2c9b4
Alexander Serraris <alexander.cs.dev@gmail.com>
2022-12-24 13:46:45 +0100

    show placeholder picture for everyone in the almanak list

--- lib/src/features/profiles/widgets/almanak_user_button_widget.dart
+++ lib/src/features/profiles/widgets/almanak_user_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/almanak.graphql.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -7,22 +8,26 @@ class AlmanakUserButtonWidget extends StatelessWidget {
   final Query$Almanak$users$data user;
 
   const AlmanakUserButtonWidget(this.user, {Key? key}) : super(key: key);
-
-  @override
+@override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          Routemaster.of(context).push('/almanak/${user.id}');
-        },
-        child: <Widget>[
-          <Widget>[
-            Text(user.fullContact.public.last_name ?? user.username),
-            const Text(', '),
-            Text(user.fullContact.public.first_name ?? ''),
-          ]
-              .toRow(mainAxisAlignment: MainAxisAlignment.start)
-              .padding(all: 15)
-              .card(elevation: 1)
-        ].toColumn().padding(horizontal: 5, vertical: 1));
+      onTap: () {
+        Routemaster.of(context).push('/almanak/${user.id}');
+      },
+      child: Card(
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundImage: Image.asset(Images.placeholderProfilePicture).image,
+          ),
+          title: Text((user.fullContact.public.first_name ?? '')+
+              ' ' +
+              (user.fullContact.public.last_name ?? '')),
+          onTap: () {
+            Routemaster.of(context).push('/almanak/${user.id}');
+          },
+        ),
+      ),
+    );
   }
+
 }
