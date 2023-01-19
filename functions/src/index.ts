bed6fd5c9c72e1e322139ab2cd2bb44102d47342
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-01-19 18:03:24 +0100

    firebase functions werkend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,14 @@ import * as functions from "firebase-functions";
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const secureFunction = functions.region("europe-west1").https;
+
+export const test = secureFunction.onCall((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  return {message: "Hello from Firebase!"};
+});
+
+export const test2 = secureFunction.onCall((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  return {message: "Hello from Firebase!"};
+});
