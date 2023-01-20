e5a60d2e899900a32052df8866b506916cbd7210
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-01-20 09:57:15 +0100

    exports defined correctly

--- functions/src/create-reservation.ts
+++ functions/src/create-reservation.ts
@@ -5,6 +5,11 @@ import * as functions from "firebase-functions";
 //
 const secureFunction = functions.region("europe-west1").https;
 
-export const createReservation = secureFunction.onCall(() => {
+export const createReservation = secureFunction.onCall((data, context) => {
+  const uid = context.auth?.uid;
+  functions
+      .logger
+      .log(`User ${uid} -> createReservation, data: ${JSON.stringify(data)}`);
+
   return {success: true};
 });
