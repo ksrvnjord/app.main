3af2bc1a7a0b79d5985e42c3213384f012eeefd7
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-01-22 10:50:34 +0100

    initial page shows name and type of reservationObject

--- lib/src/features/training/pages/show_reservation_object_page.dart
+++ lib/src/features/training/pages/show_reservation_object_page.dart
@@ -1,6 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:routemaster/routemaster.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:ksrvnjord_main_app/src/features/training/model/reservation_object.dart';
+
+// get reference to reservationObjects collection
+final CollectionReference<ReservationObject>
+    reservationObjectsCollectionReference = FirebaseFirestore.instance
+        .collection('reservationObjects')
+        .withConverter<ReservationObject>(
+            fromFirestore: (snapshot, _) =>
+                ReservationObject.fromJson(snapshot.data()!),
+            toFirestore: (reservationObject, _) => reservationObject.toJson());
 
 class ShowReservationObjectPage extends StatelessWidget {
   final String documentId;
@@ -11,14 +22,40 @@ class ShowReservationObjectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(documentId),
-        backgroundColor: Colors.lightBlue,
-        shadowColor: Colors.transparent,
-        systemOverlayStyle:
-            const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
-      ),
-      body: Center(child: Text(documentId)),
-    );
+        appBar: AppBar(
+          title: Text(documentId),
+          backgroundColor: Colors.lightBlue,
+          shadowColor: Colors.transparent,
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
+        ),
+        body: FutureWrapper(
+          future: getReservationObject(documentId),
+          success: (snapshot) {
+            if (snapshot == null || !snapshot.exists) {
+              return const Center(child: Text('No data'));
+            }
+            ReservationObject reservationObject = snapshot.data()!;
+
+            // show the reservationObject data in a ListView
+            return ListView(
+              children: [
+                ListTile(
+                  title: Text('Name'),
+                  subtitle: Text(reservationObject.name),
+                ),
+                ListTile(
+                  title: Text('Type'),
+                  subtitle: Text(reservationObject.type),
+                ),
+              ],
+            );
+          },
+        ));
+  }
+
+  Future<DocumentSnapshot<ReservationObject>> getReservationObject(
+      String documentId) async {
+    return reservationObjectsCollectionReference.doc(documentId).get();
   }
 }
