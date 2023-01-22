8cc1b4afa7a9318f79cee0371ba0890cd556b24f
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-01-22 11:54:28 +0100

    converted reservationObject constructor to use named arguments

--- lib/src/features/training/model/reservation_object.dart
+++ lib/src/features/training/model/reservation_object.dart
@@ -9,21 +9,28 @@ class ReservationObject {
   final String? comment;
   final String? kind;
 
-  const ReservationObject(this.reference, this.name, this.permissions,
-      this.type, this.available, this.comment,
-      [this.kind]);
+  // const ReservationObject(this.reference, this.name, this.permissions,
+  //     this.type, this.available, this.comment,
+  //     [this.kind]);
+  const ReservationObject(
+      {this.reference,
+      required this.name,
+      required this.permissions,
+      required this.type,
+      required this.available,
+      this.comment,
+      this.kind});
 
   ReservationObject.fromJson(Map<String, Object?> json)
-      : this(
-            json['reference'] as DocumentReference?,
-            json['name']! as String,
-            (json['permissions'] as List)
-                .map(((item) => item as String))
-                .toList(),
-            json['type']! as String,
-            json['available']! as bool,
-            json['comment'] as String?,
-            json['kind'] as String?);
+      : reference = json['reference'] as DocumentReference?,
+        name = json['name']! as String,
+        permissions = (json['permissions'] as List)
+            .map(((item) => item as String))
+            .toList(),
+        type = json['type']! as String,
+        available = json['available']! as bool,
+        comment = json['comment'] as String?,
+        kind = json['kind'] as String?;
 
   Map<String, Object?> toJson() {
     return {
