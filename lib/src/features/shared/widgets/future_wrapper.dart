bcc24069c8802d9179712e8ff1b53507b2c497cf
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-01-26 19:41:36 +0100

    improved future wrapper for easier usage. Added: initialData

--- lib/src/features/shared/widgets/future_wrapper.dart
+++ lib/src/features/shared/widgets/future_wrapper.dart
@@ -8,34 +8,35 @@ Widget onEmpty<T>(T arg) {
 class FutureWrapper<T> extends StatelessWidget {
   final Future<T> future;
   final Widget loading;
-  final Widget Function(Object? error) error;
-  final Widget Function(T? data) success;
+  final Widget Function(Object error) error;
+  final Widget Function(T data) success;
+  final T? initialData;
 
-  const FutureWrapper(
-      {Key? key,
-      required this.future,
-      required this.success,
-      this.loading = const LoadingWidget(),
-      this.error = onEmpty})
-      : super(key: key);
+  const FutureWrapper({
+    Key? key,
+    required this.future,
+    required this.success,
+    this.loading = const LoadingWidget(),
+    this.error = onEmpty,
+    this.initialData,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<T>(
       future: future,
       builder: (context, AsyncSnapshot<T> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return loading;
-        } else if (snapshot.connectionState == ConnectionState.none) {
-          return error(snapshot.data);
+        if (snapshot.hasData) {
+          // data can't be null, but the type system doesn't know that
+          // ignore: null_check_on_nullable_type_parameter
+          return success(snapshot.data!);
+        } else if (snapshot.hasError) {
+          return error(snapshot.error!);
         }
 
-        if (snapshot.hasError) {
-          return error(snapshot.data);
-        }
-
-        return success(snapshot.data);
+        return loading;
       },
+      initialData: initialData,
     );
   }
 }
