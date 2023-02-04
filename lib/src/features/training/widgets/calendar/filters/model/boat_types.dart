d614826442634326bb347cdb07a9a01e6d7e08cf
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-04 11:51:48 +0100

    added type groups

---
+++ lib/src/features/training/widgets/calendar/filters/model/boat_types.dart
@@ -0,0 +1,33 @@
+Map<String, List<String>> reservationObjectTypes = {
+  'Binnen': [
+    'Ruimtes', // In App
+    'Ergometers', // Ecaptain
+    'Fietsen', // Ecaptain
+    'Mat', // Ecaptain
+  ],
+  '1 roeier': [
+    'C1x', // In App
+    '1x', // Ecaptain
+  ],
+  '2 roeiers': [
+    'C2x', // In App
+    'C2+', // In App
+    'C2x+', // In App
+    '2-', // Ecaptain
+    '2x', // Ecaptain
+    '2+', // Ecaptain
+  ],
+  '4 roeiers': [
+    'C4+', // In App
+    '4+', // Ecaptain
+    '4x', // Ecaptain
+  ],
+  '8 roeiers': [
+    '8+', // Ecaptain
+  ],
+  'Overig': [
+    'Scull riemen', // Ecaptain
+    'Cox', // Ecaptain
+    'Motorboot', // Ecaptain
+  ]
+};
