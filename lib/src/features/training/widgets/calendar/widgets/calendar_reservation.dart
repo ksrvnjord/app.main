565bc41dedbed9d32f07c381e9950b0f16b7e22b
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-04 16:50:53 +0100

    fix

--- lib/src/features/training/widgets/calendar/widgets/calendar_reservation.dart
+++ lib/src/features/training/widgets/calendar/widgets/calendar_reservation.dart
@@ -17,45 +17,40 @@ class CalendarReservation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (data != null) {
-      // Calculate the offset of the reservation
-      DateTime timestamp = (data['startTime'] as Timestamp).toDate();
-      Duration offset = timestamp.difference(
-        DateTime(timestamp.year, timestamp.month, timestamp.day, 6, 0, 0),
-      );
+    DateTime timestamp = (data['startTime'] as Timestamp).toDate();
+    Duration offset = timestamp.difference(
+      DateTime(timestamp.year, timestamp.month, timestamp.day, 6, 0, 0),
+    );
 
-      // Calculate the duration of the reservation
-      double duration = ((data['endTime'] as Timestamp).seconds -
-              (data['startTime'] as Timestamp).seconds) /
-          3600;
+    // Calculate the duration of the reservation
+    double duration = ((data['endTime'] as Timestamp).seconds -
+            (data['startTime'] as Timestamp).seconds) /
+        3600;
 
-      final navigator = Routemaster.of(context);
+    final navigator = Routemaster.of(context);
 
-      return [
-        GestureDetector(
-          onTap: () {
-            id != null ? navigator.push(id!) : null;
-          },
-          child: Container(
-            width: 128,
-            height: duration * 32 * 2,
-            decoration: const BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: BorderRadius.all(Radius.circular(6)),
-            ),
-            child: Text(
-              data['creatorName'] ?? 'Afschrijving',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-              ),
-            ).padding(all: 4),
+    return [
+      GestureDetector(
+        onTap: () {
+          id != null ? navigator.push(id!) : null;
+        },
+        child: Container(
+          width: 128,
+          height: duration * 32 * 2,
+          decoration: const BoxDecoration(
+            color: Colors.blueGrey,
+            borderRadius: BorderRadius.all(Radius.circular(6)),
           ),
+          child: Text(
+            data['creatorName'] ?? 'Afschrijving',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ).padding(all: 4),
         ),
-      ].toColumn().padding(top: 16 + ((offset.inMinutes / 30) * 32));
-    }
-
-    return Container();
+      ),
+    ].toColumn().padding(top: 16 + ((offset.inMinutes / 30) * 32));
   }
 }
