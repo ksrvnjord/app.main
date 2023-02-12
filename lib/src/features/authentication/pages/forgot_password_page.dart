ef572339e1187238833de98f90b46e2a9ade26b2
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-12 13:20:12 +0100

    split widgets over multiple files. to reduces widget count per file

--- lib/src/features/authentication/pages/forgot_password_page.dart
+++ lib/src/features/authentication/pages/forgot_password_page.dart
@@ -1,59 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
+import 'package:ksrvnjord_main_app/src/features/authentication/widgets/forgot_form_card.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/logo_widget.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
-
-class _ForgotFormCard extends StatelessWidget {
-  const _ForgotFormCard({Key? key, required this.loginCallback})
-      : super(key: key);
-  final void Function(bool) loginCallback;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: Implement in Heimdall
-    // return const ForgotForm();
-    const double textPadding = 16;
-
-    const double buttonHeight = 54;
-    const double buttonPadding = 16;
-    const double cardElevation = 8;
-    const double cardOuterPadding = 16;
-    const double cardInnerPadding = 24;
-    const double textSize = 16;
-
-    return [
-      const Text(
-        'Op dit moment kan een wachtwoord alleen gereset worden via de website',
-        textAlign: TextAlign.center,
-      ).fontSize(textSize).padding(vertical: textPadding),
-      [
-        ElevatedButton(
-          child: const Text('Ga naar de website').fontSize(textSize),
-          onPressed: () => Routemaster.of(context).push('/forgot/webview'),
-        ).height(buttonHeight).padding(right: buttonPadding).expanded(),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
-          child: const Icon(Icons.arrow_back),
-          onPressed: () => Routemaster.of(context).pop(),
-        ).height(buttonHeight),
-      ].toRow(),
-    ]
-        .toColumn()
-        .padding(all: cardInnerPadding)
-        .card(
-          elevation: cardElevation,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-        )
-        .padding(all: cardOuterPadding)
-        .alignment(Alignment.center);
-  }
-}
-
-// ignore: no-empty-block
-void dontCall(bool _) {}
 
 class ForgotPasswordPage extends StatelessWidget {
   final void Function(bool) loginCallback;
@@ -70,10 +20,13 @@ class ForgotPasswordPage extends StatelessWidget {
       backgroundColor: Colors.lightBlue,
       body: <Widget>[
         const LogoWidget(image: Images.appLogo).padding(bottom: logoPadding),
-        _ForgotFormCard(
+        ForgotFormCard(
           loginCallback: loginCallback,
         ),
       ].toColumn(mainAxisAlignment: MainAxisAlignment.center),
     );
   }
 }
+
+// ignore: no-empty-block
+void dontCall(bool _) {}
--- lib/src/features/authentication/pages/register_page.dart
+++ lib/src/features/authentication/pages/register_page.dart
@@ -1,61 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
+import 'package:ksrvnjord_main_app/src/features/authentication/widgets/register_form_card.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/logo_widget.dart';
-import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
 
-class _RegisterFormCard extends StatelessWidget {
-  const _RegisterFormCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    const double textPadding = 16;
-
-    const double buttonHeight = 54;
-    const double buttonPadding = 16;
-    const double cardElevation = 8;
-    const double cardOuterPadding = 16;
-    const double cardInnerPadding = 24;
-    const double textSize = 16;
-
-    return [
-      const Text(
-        'Op dit moment kan een account alleen aangemaakt worden via de website',
-        textAlign: TextAlign.center,
-      ).fontSize(textSize).padding(vertical: textPadding),
-      [
-        ElevatedButton(
-          child: const Text('Ga naar de website').fontSize(textSize),
-          onPressed: () => Routemaster.of(context).push('/register/webview'),
-        ).height(buttonHeight).padding(right: buttonPadding).expanded(),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
-          child: const Icon(Icons.arrow_back),
-          onPressed: () => Routemaster.of(context).pop(),
-        ).height(buttonHeight),
-      ].toRow(),
-    ]
-        .toColumn()
-        .padding(all: cardInnerPadding)
-        .card(
-          elevation: cardElevation,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-        )
-        .padding(all: cardOuterPadding)
-        .alignment(Alignment.center);
-  }
-}
-
-// ignore: no-empty-block
-void dontCall(bool _) {}
-
 class RegisterPage extends StatelessWidget {
-  final void Function(bool) loginCallback;
-
-  const RegisterPage({Key? key, this.loginCallback = dontCall})
-      : super(key: key);
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +16,7 @@ class RegisterPage extends StatelessWidget {
       backgroundColor: Colors.lightBlue,
       body: <Widget>[
         const LogoWidget(image: Images.appLogo).padding(bottom: logoPadding),
-        const _RegisterFormCard(),
+        const RegisterFormCard(),
       ].toColumn(mainAxisAlignment: MainAxisAlignment.center),
     );
   }
