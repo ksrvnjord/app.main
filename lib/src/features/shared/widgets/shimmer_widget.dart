f8e389d01a23cc2f418b1cd625c7d0c2bfef7d85
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-12 13:31:20 +0100

    shimmer should have child, otherwise you see nothing

--- lib/src/features/shared/widgets/shimmer_widget.dart
+++ lib/src/features/shared/widgets/shimmer_widget.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerWidget extends StatelessWidget {
-  const ShimmerWidget({super.key, this.child});
-  final Widget? child;
+  const ShimmerWidget({super.key, required this.child});
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Shimmer.fromColors(
       baseColor: Colors.grey[300]!,
       highlightColor: Colors.grey[100]!,
-      child: child ?? Container(),
+      child: child,
     );
   }
 }
