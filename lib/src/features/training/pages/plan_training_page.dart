6bf58747d42b5e1a0ad06d3623f7696eb0fdb5d8
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-13 22:48:00 +0100

    linter

--- lib/src/features/training/pages/plan_training_page.dart
+++ lib/src/features/training/pages/plan_training_page.dart
@@ -274,8 +274,8 @@ class _PlanTrainingPageState extends State<PlanTrainingPage> {
               },
               style: ElevatedButton.styleFrom(
                 // add rounding
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
                 ),
                 backgroundColor: Colors.lightBlue,
               ),
