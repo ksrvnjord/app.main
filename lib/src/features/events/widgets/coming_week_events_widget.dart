2c1ddea62938006d0c041e90539802dca5e46126
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-15 11:12:38 +0100

    refactor

--- lib/src/features/events/widgets/coming_week_events_widget.dart
+++ lib/src/features/events/widgets/coming_week_events_widget.dart
@@ -9,6 +9,8 @@ import 'package:provider/provider.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:intl/intl.dart';
 
+import 'upcoming_event_widget.dart';
+
 class ComingWeekEventsWidget extends StatelessWidget {
   const ComingWeekEventsWidget({super.key});
 
@@ -71,56 +73,6 @@ class ComingWeekEventsWidget extends StatelessWidget {
               );
             },
           ),
-        ),
-      ],
-    );
-  }
-}
-
-class UpcomingEventWidget extends StatelessWidget {
-  const UpcomingEventWidget({
-    super.key,
-    required this.elementPadding,
-    required this.event,
-  });
-
-  final double elementPadding;
-  final Event event;
-
-  @override
-  Widget build(BuildContext context) {
-    const double topPadding = 8;
-    const double dayNumberFontSize = 24;
-    DateFormat monthFormat = DateFormat('MMM EEE', 'nl_NL');
-    const double dateFontSize = 12;
-    DateFormat timeFormat = DateFormat('HH:mm');
-    const int maxLines = 2;
-    const double eventFontSize = 16;
-    DateTime start = event.startTime;
-
-    return Row(
-      children: [
-        Text(start.day.toString())
-            .fontSize(dayNumberFontSize)
-            .paddingDirectional(horizontal: elementPadding),
-        Text(
-          monthFormat.format(start),
-        )
-            .fontSize(dateFontSize)
-            .textColor(Colors.blueGrey)
-            .padding(top: topPadding),
-        Text(timeFormat.format(start))
-            .fontSize(dateFontSize)
-            .textColor(Colors.blueGrey)
-            .padding(horizontal: elementPadding, top: topPadding),
-        Expanded(
-          child: Text(
-            event.title,
-            maxLines: maxLines,
-            overflow: TextOverflow.ellipsis,
-          )
-              .fontSize(eventFontSize)
-              .paddingDirectional(horizontal: elementPadding),
         ),
       ],
     );
---
+++ lib/src/features/events/widgets/upcoming_event_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ksrvnjord_main_app/src/features/events/models/event.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+class UpcomingEventWidget extends StatelessWidget {
+  const UpcomingEventWidget({
+    super.key,
+    required this.elementPadding,
+    required this.event,
+  });
+
+  final double elementPadding;
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    const double topPadding = 8;
+    const double dayNumberFontSize = 24;
+    DateFormat monthFormat = DateFormat('MMM EEE', 'nl_NL');
+    const double dateFontSize = 12;
+    DateFormat timeFormat = DateFormat('HH:mm');
+    const int maxLines = 2;
+    const double eventFontSize = 16;
+    DateTime start = event.startTime;
+
+    return Row(
+      children: [
+        Text(start.day.toString())
+            .fontSize(dayNumberFontSize)
+            .paddingDirectional(horizontal: elementPadding),
+        Text(
+          monthFormat.format(start),
+        )
+            .fontSize(dateFontSize)
+            .textColor(Colors.blueGrey)
+            .padding(top: topPadding),
+        Text(timeFormat.format(start))
+            .fontSize(dateFontSize)
+            .textColor(Colors.blueGrey)
+            .padding(horizontal: elementPadding, top: topPadding),
+        Expanded(
+          child: Text(
+            event.title,
+            maxLines: maxLines,
+            overflow: TextOverflow.ellipsis,
+          )
+              .fontSize(eventFontSize)
+              .paddingDirectional(horizontal: elementPadding),
+        ),
+      ],
+    );
+  }
+}
