6179a3df38ad80201b6c493daa350e57b026cd1e
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-15 10:02:34 +0100

    place header oudside of card

--- lib/src/features/events/widgets/coming_week_events_widget.dart
+++ lib/src/features/events/widgets/coming_week_events_widget.dart
@@ -5,6 +5,7 @@ import 'package:ksrvnjord_main_app/src/features/events/models/events.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class ComingWeekEventsWidget extends StatelessWidget {
   const ComingWeekEventsWidget({super.key});
@@ -14,41 +15,54 @@ class ComingWeekEventsWidget extends StatelessWidget {
     GraphQLClient client = Provider.of<GraphQLModel>(context).client;
     final eventsData = events(client);
 
-    return Card(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      child: FutureWrapper(
-        future: eventsData,
-        success: (events) {
-          // filter all events that are in the coming week
-          Iterable<Query$CalendarItems$events?> comingWeekEvents =
-              events.where((event) {
-            if (event == null || event.start_time == null) {
-              return false;
-            }
-            final DateTime start = DateTime.parse(event.start_time!);
+    const double titleFontSize = 16;
 
-            return start.isBefore(
-                  DateTime.now().add(const Duration(days: 30)),
-                ) &&
-                start.isAfter(DateTime.now());
-          });
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: const Text(
+            "Evenementen komende week",
+          ).fontSize(titleFontSize).fontWeight(FontWeight.w600),
+        ),
+        Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          child: FutureWrapper(
+            future: eventsData,
+            success: (events) {
+              // filter all events that are in the coming week
+              Iterable<Query$CalendarItems$events?> comingWeekEvents =
+                  events.where((event) {
+                if (event == null || event.start_time == null) {
+                  return false;
+                }
+                final DateTime start = DateTime.parse(event.start_time!);
 
-          return Column(
-            children: [
-              ...comingWeekEvents.map((event) {
-                return event != null
-                    ? ListTile(
-                        title: Text(event.title!),
-                        subtitle: Text(event.start_time!),
-                      )
-                    : Container();
-              }),
-            ],
-          );
-        },
-      ),
+                return start.isBefore(
+                      DateTime.now().add(const Duration(days: 30)),
+                    ) &&
+                    start.isAfter(DateTime.now());
+              });
+
+              return Column(
+                children: [
+                  ...comingWeekEvents.map((event) {
+                    return event != null
+                        ? ListTile(
+                            title: Text(event.title!),
+                            subtitle: Text(event.start_time!),
+                          )
+                        : Container();
+                  }),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
