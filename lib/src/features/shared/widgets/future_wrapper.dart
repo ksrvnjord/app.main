7312cd30b2266e82cb81cdaba55b3935dc6738db
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-16 13:18:52 +0100

    ignore

--- lib/src/features/shared/widgets/future_wrapper.dart
+++ lib/src/features/shared/widgets/future_wrapper.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/loading_widget.dart';
 
+// No other idea than to use this function
+// ignore: avoid-returning-widgets
 Widget onEmpty<T>(T arg) {
   return Text(arg.toString());
 }
