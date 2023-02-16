4d5e66e127bae50179ef6f26c75793e109d032cf
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-16 13:10:32 +0100

    remove unused imports

--- lib/src/features/profiles/pages/almanak_page.dart
+++ lib/src/features/profiles/pages/almanak_page.dart
@@ -1,16 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_picture.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/widgets/almanak_user_button_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/almanak_widget.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/widgets/default_profile_picture.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/my_profile_picture.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/shimmer_widget.dart';
 import 'package:provider/provider.dart';
-import 'package:routemaster/routemaster.dart';
 
 class AlmanakPage extends StatelessWidget {
   const AlmanakPage({Key? key}) : super(key: key);
