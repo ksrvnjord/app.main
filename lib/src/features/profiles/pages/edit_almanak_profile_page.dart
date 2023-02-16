7ef404397aad51bf263ce804c05bcbbfe9cf1ba8
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-02-16 20:40:16 +0100

    make room for additional profile fields

--- lib/src/features/profiles/pages/edit_almanak_profile_page.dart
+++ lib/src/features/profiles/pages/edit_almanak_profile_page.dart
@@ -5,6 +5,8 @@ import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_picture.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/edit_profile_picture_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:routemaster/routemaster.dart';
 
 final storage = FirebaseStorage.instance;
@@ -41,29 +43,10 @@ class _EditAlmanakProfilePageState extends State<EditAlmanakProfilePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: FutureBuilder(
-            future: getMyProfilePicture(),
-            builder: (context, snapshot) {
-              Image initialImage;
-
-              if (snapshot.hasError) {
-                initialImage = Image.asset(Images.placeholderProfilePicture);
-
-                return EditProfilePictureWidget(
-                  initialImage: initialImage,
-                );
-              } else if (snapshot.hasData) {
-                initialImage = Image.memory(snapshot.data as Uint8List);
-
-                return EditProfilePictureWidget(
-                  initialImage: initialImage,
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          ),
+        child: ListView(
+          children: const [
+            Center(child: EditProfilePictureWidget()),
+          ],
         ),
       ),
     );
--- lib/src/features/profiles/widgets/edit_profile_picture_widget.dart
+++ lib/src/features/profiles/widgets/edit_profile_picture_widget.dart
@@ -1,30 +1,34 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker_widget/image_picker_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_picture.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 
 class EditProfilePictureWidget extends StatelessWidget {
   const EditProfilePictureWidget({
     Key? key,
-    required this.initialImage,
   }) : super(key: key);
-
-  final Image initialImage;
 
   @override
   Widget build(BuildContext context) {
-    const double profilePictureSize = 320;
+    const double profilePictureSize = 240;
 
-    return ImagePickerWidget(
-      diameter: profilePictureSize,
-      initialImage: initialImage.image,
-      shape: ImagePickerWidgetShape.circle, // ImagePickerWidgetShape.square
-      isEditable: true,
-      onChange: tryUploadProfilePicture,
+    return FutureWrapper(
+      future: getMyProfilePicture(),
+      success: (snapshot) => ImagePickerWidget(
+        diameter: profilePictureSize,
+        initialImage: Image.memory(snapshot as Uint8List).image,
+        shape: ImagePickerWidgetShape.circle, // ImagePickerWidgetShape.square
+        isEditable: true,
+        onChange: tryUploadProfilePicture,
+      ),
+      error: (error) => ErrorCardWidget(errorMessage: error.toString()),
     );
   }
 
