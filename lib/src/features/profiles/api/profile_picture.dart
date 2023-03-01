bddbe9fafb9756b4e7f459b63fc77b1a5a939f6a
Max Boone <max@netpunt.nl>
2023-03-01 21:54:28 +0100

    DRY & Prepare for caching layer

--- lib/src/features/profiles/api/profile_picture.dart
+++ lib/src/features/profiles/api/profile_picture.dart
@@ -6,18 +6,47 @@ import 'package:firebase_storage/firebase_storage.dart';
 final storage = FirebaseStorage.instance;
 final auth = FirebaseAuth.instance;
 
-Future<Uint8List?> getProfilePicture(String userId) async {
-  final Reference userRef = storage.ref().child(userId);
-  final Reference profilePictureRef = userRef.child('profile_picture.png');
-
-  return await profilePictureRef.getData();
+Reference getProfilePictureRef(String userId) {
+  // Get the profile picture from a user's folder, all avatars are
+  // with this filename.
+  return storage.ref().child('$userId/profile_picture.png');
 }
 
-Future<String> getProfilePictureUrl(String userId) async {
-  final Reference userRef = storage.ref().child(userId);
-  final Reference profilePictureRef = userRef.child('profile_picture.png');
+Future<Uint8List?> getProfilePicture(String userId) async {
+  // Define the variable before populating it
+  Uint8List? picture;
 
-  return await profilePictureRef.getDownloadURL();
+  // Get the profile picture using the reference
+  try {
+    picture = await getProfilePictureRef(userId).getData();
+  } on FirebaseException catch (e) {
+    // Check if the issue is a 404, if not, do throw.
+    if (e.code != 'object-not-found') {
+      rethrow;
+    }
+  }
+
+  return picture;
+}
+
+Future<String?> getProfilePictureUrl(String userId) async {
+  // Define the variable before populating it
+  String? picture;
+
+  // Get the profile picture using the reference
+  try {
+    picture = await getProfilePictureRef(userId).getDownloadURL();
+  } on FirebaseException catch (e) {
+    // Check if the issue is a 404, if not, do throw.
+    if (e.code == 'object-not-found') {
+      return null;
+    }
+
+    // Likely a legit issue, so rethrow to raise the exception to Sentry
+    rethrow;
+  }
+
+  return picture;
 }
 
 // Make function to getMyProfilePicture
@@ -26,9 +55,5 @@ Future<Uint8List?> getMyProfilePicture() {
 }
 
 UploadTask uploadMyProfilePicture(File file) {
-  final String userId = auth.currentUser!.uid;
-  final Reference myProfilePictureRef =
-      storage.ref("$userId/profile_picture.png");
-
-  return myProfilePictureRef.putFile(file);
+  return getProfilePictureRef(auth.currentUser!.uid).putFile(file);
 }
--- lib/src/features/profiles/widgets/profile_picture_widget.dart
+++ lib/src/features/profiles/widgets/profile_picture_widget.dart
@@ -18,16 +18,18 @@ class ProfilePictureWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureWrapper(
-      future: getProfilePictureUrl(userId), // TODO: use cache
-      success: (snapshot) => CachedNetworkImage(
-        imageUrl: // random image url
-            snapshot,
-        imageBuilder: (context, imageProvider) => CircleAvatar(
-          radius: size,
-          backgroundImage: imageProvider,
-        ),
-        placeholder: (_, x) => DefaultProfilePicture(radius: size),
-      ),
+      future: getProfilePictureUrl(userId),
+      success: (snapshot) => snapshot != null
+          ? CachedNetworkImage(
+              cacheKey: '$userId-profilePicture',
+              imageUrl: snapshot,
+              imageBuilder: (context, imageProvider) => CircleAvatar(
+                radius: size,
+                backgroundImage: imageProvider,
+              ),
+              placeholder: (_, x) => DefaultProfilePicture(radius: size),
+            )
+          : DefaultProfilePicture(radius: size),
       error: (_) => DefaultProfilePicture(radius: size),
       loading: ShimmerWidget(child: DefaultProfilePicture(radius: size)),
     );
