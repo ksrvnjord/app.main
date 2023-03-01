5f204ff13139d0d2361a6a3df98656f5575c30c0
Max Boone <max@netpunt.nl>
2023-03-01 23:55:07 +0100

    Move to a more logical directory

--- lib/src/features/shared/model/hive_cached_image.dart
+++ lib/src/features/shared/model/hive_cached_image.dart
@@ -5,20 +5,32 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 
+// Default expiry is one hour
+const defaultExpiry = 1;
+
 // TODO: NEEDS TO BE REWRITTEN INTO AN OBJECT-ORIENTED CLASS,
 // USE CASE FOR THAT WILL PROBABLY SURFACE IN A COUPLE WEEKS OR
 // SO.
 Future<Uint8List?> cachedHttpImage(
-  String url,
-  Duration expire,
-) async {
+  String url, {
+  Duration expire = const Duration(hours: defaultExpiry),
+}) async {
+  // If the URL is empty, we don't need to do anything
+  // at all.
+  if (url == '') {
+    return null;
+  }
+
+  // Create the expiry DateTime
+  DateTime expireDate = DateTime.now().add(expire);
+
   // Initialize a Hive-cache to store the images, do it lazily
   // as we don't want to load all the images right away
   LazyBox cache = await Hive.openLazyBox('imageCache');
 
-  // Normalize the URL to a SHA256 hash to avoid
+  // Normalize the URL to a SHA224 hash to avoid
   // key length issues
-  Digest key = sha256.convert(utf8.encode(url));
+  Digest key = sha512.convert(utf8.encode(url));
 
   // Initialize variable to store the output in
   // from the try-catch block
@@ -26,10 +38,12 @@ Future<Uint8List?> cachedHttpImage(
 
   // Check if we have an item in our cache,
   // and then check if we should use it
-  var item = await cache.get(key);
+  var item = await cache.get(key.toString());
 
   // We have it, so return it.
-  if (item['expire'] != null && DateTime.now().isBefore(item['expire'])) {
+  if (item != null &&
+      item['expire'] != null &&
+      DateTime.now().isBefore(item['expire'])) {
     return item['data'];
   }
 
@@ -40,8 +54,8 @@ Future<Uint8List?> cachedHttpImage(
       options: Options(responseType: ResponseType.bytes),
     );
     output = response.data;
-    cache.put(key, {
-      'expire': DateTime.now().add(expire),
+    cache.put(key.toString(), {
+      'expire': expireDate,
       'data': output,
     });
   } on DioError catch (e) {
