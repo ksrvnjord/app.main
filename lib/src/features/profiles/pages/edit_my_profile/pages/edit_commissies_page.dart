325bb828cf0ef4217ce9e66b6ac36b30970fb134
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-03 10:02:03 +0100

    show commissies from firestore

--- lib/src/features/profiles/pages/edit_my_profile/pages/edit_commissies_page.dart
+++ lib/src/features/profiles/pages/edit_my_profile/pages/edit_commissies_page.dart
@@ -1,7 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/pages/edit_my_profile/models/commissie_entry.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
+
+final peopleRef = FirebaseFirestore.instance.collection("people");
+final DocumentReference<Map<String, dynamic>> user =
+    peopleRef.doc(FirebaseAuth.instance.currentUser!.uid);
+final CollectionReference<CommissieEntry> userCommissiesRef = user
+    .collection("commissies")
+    .withConverter<CommissieEntry>(
+      fromFirestore: (snapshot, _) => CommissieEntry.fromJson(snapshot.data()!),
+      toFirestore: (commissie, _) => commissie.toJson(),
+    );
 
 class EditCommissiesPage extends StatefulWidget {
   const EditCommissiesPage({Key? key}) : super(key: key);
@@ -14,6 +28,12 @@ class EditCommissiesPageState extends State<EditCommissiesPage> {
   static const double fieldPadding = 8;
   static const double titleFontSize = 20;
 
+  Future<List<QueryDocumentSnapshot<CommissieEntry>>> getCommissies() async {
+    QuerySnapshot<CommissieEntry> snapshot = await userCommissiesRef.get();
+
+    return snapshot.docs;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,36 +44,28 @@ class EditCommissiesPageState extends State<EditCommissiesPage> {
         systemOverlayStyle:
             const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
       ),
-      body: ListView(
-        children: <Widget>[
-          const Text('Mijn Commissies')
-              .textColor(Colors.blueGrey)
-              .fontSize(titleFontSize)
-              .padding(all: fieldPadding),
-          // TODO: ophalen van commissies subcollection in firestore
-          ListTile(
-            leading: const [Text("2022-2023")]
-                .toColumn(mainAxisAlignment: MainAxisAlignment.center),
-            title: const Text('App Commissie'),
-            subtitle: const Text("Ab-actis"),
-            trailing: const Icon(Icons.delete),
-          ),
-          // another list tile with the same structure
-          ListTile(
-            leading: [const Text("2022-2023")]
-                .toColumn(mainAxisAlignment: MainAxisAlignment.center),
-            title: const Text('App Commissie'),
-            subtitle: const Text("Ab-actis"),
-            trailing: const Icon(Icons.delete),
-          ),
-          ListTile(
-            leading: const [Text("2022-2023")]
-                .toColumn(mainAxisAlignment: MainAxisAlignment.center),
-            title: const Text('App Commissie'),
-            trailing: const Icon(Icons.delete),
-          ),
-        ],
-      ),
+      body: ListView(children: [
+        const Text('Mijn Commissies')
+            .textColor(Colors.blueGrey)
+            .fontSize(titleFontSize)
+            .padding(all: fieldPadding),
+        FutureWrapper(
+          future: getCommissies(),
+          success: (commissies) => commissies
+              .map((doc) => ListTile(
+                    leading: [
+                      Text("${doc.data().startYear}-${doc.data().endYear}"),
+                    ].toColumn(mainAxisAlignment: MainAxisAlignment.center),
+                    title: Text(doc.data().name),
+                    subtitle: doc.data().function != null
+                        ? Text(doc.data().function!)
+                        : null,
+                    trailing: const Icon(Icons.delete),
+                  ))
+              .toList()
+              .toColumn(),
+        ),
+      ]),
       floatingActionButton: // button with a plus icon and the text "Commissie"
           FloatingActionButton.extended(
         onPressed: () => Routemaster.of(context).push('select'),
