7110cbb523b54b49e533c82452da2ad682f436f9
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-04 20:14:42 +0100

    basic UI voor bestuur

--- lib/src/features/profiles/pages/almanak_bestuur_page.dart
+++ lib/src/features/profiles/pages/almanak_bestuur_page.dart
@@ -1,15 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/widgets/almanak_searchable_list_widget.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
-import 'package:provider/provider.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/models/almanak_profile.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/widgets/profile_picture_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:styled_widget/styled_widget.dart';
+
+final peopleRef = FirebaseFirestore.instance.collection("people").withConverter(
+      fromFirestore: (snapshot, _) => AlmanakProfile.fromJson(snapshot.data()!),
+      toFirestore: (almanakProfile, _) => almanakProfile.toJson(),
+    );
 
 class AlmanakBestuurPage extends StatelessWidget {
   const AlmanakBestuurPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final client = Provider.of<GraphQLModel>(context).client;
+    Future<QuerySnapshot<AlmanakProfile>> getBestuur() {
+      return peopleRef.where("bestuurs_functie", isNull: false).get();
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -22,7 +31,21 @@ class AlmanakBestuurPage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(8),
         children: [
-          Text("lijst met bestuur"),
+          FutureWrapper(
+            future: getBestuur(),
+            success: (snapshot) => <Widget>[
+              ...snapshot.docs.map(
+                (doc) => ListTile(
+                  leading: ProfilePictureWidget(userId: doc.id),
+                  title:
+                      Text("${doc.data().firstName!} ${doc.data().lastName!}"),
+                  subtitle: Text(doc.data().bestuursFunctie!),
+                  trailing:
+                      const Icon(Icons.chevron_right, color: Colors.lightBlue),
+                ),
+              ),
+            ].toColumn(),
+          ),
         ],
       ),
     );
