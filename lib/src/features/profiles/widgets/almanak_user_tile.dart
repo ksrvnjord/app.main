ce7ee75ba915e51ab1d099f56301dc7beba3bca5
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-05 14:15:01 +0100

    push to route with hiemdall Id

--- lib/src/features/profiles/widgets/almanak_user_tile.dart
+++ lib/src/features/profiles/widgets/almanak_user_tile.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/models/profile.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/profile_picture_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
 
 class AlmanakUserTile extends StatelessWidget {
@@ -16,10 +20,17 @@ class AlmanakUserTile extends StatelessWidget {
   final String lidnummer;
 
   @override
-  Widget build(BuildContext context) => ListTile(
+  Widget build(BuildContext context) {
+    final client = Provider.of<GraphQLModel>(context).client;
+
+    return FutureWrapper(
+      future: almanakProfileByIdentifier(lidnummer, client),
+      success: (heimdallUser) => ListTile(
         leading: ProfilePictureWidget(userId: lidnummer),
         title: Text("$firstName $lastName"),
         subtitle: subtitle != null ? Text(subtitle!) : null,
-        onTap: () => Routemaster.of(context).push(lidnummer),
-      );
+        onTap: () => Routemaster.of(context).push(heimdallUser!.id),
+      ),
+    );
+  }
 }
