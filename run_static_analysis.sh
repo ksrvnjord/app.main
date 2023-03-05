6638a062646f25b5b1669eb8c0472c944394a4b6
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-05 13:17:43 +0100

    run this script to run all the static analysis commands so you don't have to wait for slow GitHub Actions

---
+++ run_static_analysis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Can't run the script? Try `chmod +x run_static_analysis.sh` to make it executable.
+
+# This script runs the static analysis tools on the codebase.
+
+# create an array containing the commands
+commands=(
+    "flutter analyze"
+    "dart format --set-exit-if-changed ."
+    "flutter pub run dart_code_metrics:metrics analyze lib --fatal-style --fatal-performance --fatal-warnings --reporter=console"
+    "flutter pub run dart_code_metrics:metrics check-unused-files --exclude=/**.graphql.dart lib"
+    "flutter pub run dart_code_metrics:metrics check-unnecessary-nullable lib"
+)
+
+# loop through the array and execute each command
+for command in "${commands[@]}"; do
+    echo ""
+    echo "Running command: $command"
+    $command # run the command
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then # exit if the command fails
+        echo "❌ Static analysis failed for '$command'"
+        exit $EXIT_CODE
+    fi
+    echo "✅ Static analysis passed for '$command'"
+done
+echo ""
+echo "✅ Static analysis completed successfully, good job!"
