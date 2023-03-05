bf9b519ea98d64af95e92f08bad95be7b6a7f8a5
Max Boone <max@netpunt.nl>
2023-03-06 00:03:50 +0100

    Write CollectionQuery

--- lib/src/features/damages/queries/all_damages.dart
+++ lib/src/features/damages/queries/all_damages.dart
@@ -1,5 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ksrvnjord_main_app/src/features/damages/model/damage.dart';
 
-Future<List<Damage>> damages() async {
-  return [];
+final FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<List<QueryDocumentSnapshot<Damage>>> allDamages(
+  String? reservationObjectId,
+) async {
+  var damagesQuery = db.collectionGroup("damages").withConverter<Damage>(
+        fromFirestore: (snapshot, _) => Damage.fromJson(snapshot.data()!),
+        toFirestore: (reservation, _) => reservation.toJson(),
+      );
+
+  if (reservationObjectId != null) {
+    damagesQuery = damagesQuery.where('parent', isEqualTo: reservationObjectId);
+  }
+
+  final damages = await damagesQuery.get();
+
+  return damages.docs;
 }
