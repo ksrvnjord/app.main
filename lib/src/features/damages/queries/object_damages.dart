41eacc4539990c06fc382c14b66e69832a69ebd8
Max Boone <max@netpunt.nl>
2023-03-06 14:53:44 +0100

    Delete unused file and re-format file

--- lib/src/features/damages/queries/object_damages.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ksrvnjord_main_app/src/features/damages/model/damage.dart';
-
-Future<List<Damage>> objectDamages() async {
-  return [];
-}
--- lib/src/features/training/queries/get_reservation_object.dart
+++ lib/src/features/training/queries/get_reservation_object.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservation_object.dart';
 
