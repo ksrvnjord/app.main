283aa10eb0be24a217ce7026a80e78a3e1322361
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-25 14:45:58 +0100

    query heimdall and Firestore simultaneous for faster load times

--- lib/src/features/profiles/pages/almanak_profile/almanak_profile_page.dart
+++ lib/src/features/profiles/pages/almanak_profile/almanak_profile_page.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/models/profile.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/pages/almanak_profile/widgets/almanak_user_profile_view.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
 import 'package:routemaster/routemaster.dart';
-import '../../../shared/model/graphql_model.dart';
 
 class AlmanakProfilePage extends ConsumerWidget {
   const AlmanakProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Map<String, String> params = RouteData.of(context).pathParameters;
-    final client = ref.watch(graphQLModelProvider).client;
+    final Map<String, String> params = RouteData.of(context).pathParameters;
+    final String? identifier = params['identifier']; // lidnummer of user
 
     return Scaffold(
       appBar: AppBar(
@@ -24,13 +21,8 @@ class AlmanakProfilePage extends ConsumerWidget {
         systemOverlayStyle:
             const SystemUiOverlayStyle(statusBarColor: Colors.lightBlue),
       ),
-      body: params['identifier'] != null
-          ? FutureWrapper(
-              // we first need to query the user to get the userId
-              future: almanakProfileByIdentifier(params['identifier']!, client),
-              success: (user) => AlmanakUserProfileView(heimdallUser: user!),
-              error: (error) => ErrorCardWidget(errorMessage: error.toString()),
-            )
+      body: identifier != null
+          ? AlmanakUserProfileView(identifier: identifier)
           : const ErrorCardWidget(errorMessage: 'Geen profiel gevonden'),
     );
   }
