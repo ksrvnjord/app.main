cd664a216dfdb2ac896c092b86774209b9c58728
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-27 23:55:28 +0200

    simplify

--- lib/src/features/training/widgets/calendar/calendar_overview.dart
+++ lib/src/features/training/widgets/calendar/calendar_overview.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/training/api/reservation_object_type_filters_notifier.dart';
-import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/filters/api/reservation_objects.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/widgets/time_scrollview.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/widgets/vertical_reservation_scrollview_with_sticky_header.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -50,47 +48,29 @@ class _CalendarOverview extends ConsumerState<CalendarOverview> {
   Widget build(BuildContext context) {
     final List<String> filters = ref.watch(reservationTypeFiltersListProvider);
 
-    if (filters.isEmpty) {
-      return <Widget>[
-        const Icon(Icons.waves, color: Colors.blueGrey)
-            .padding(all: iconPadding),
-        const Text(
-          'Selecteer een categorie om te beginnen',
-          style: TextStyle(color: Colors.blueGrey),
-        ),
-      ].toColumn(mainAxisAlignment: MainAxisAlignment.center);
-    }
-
-    DateTime date = widget.date;
-
-    final availableReservationObjects =
-        ref.watch(availableReservationObjectsProvider);
-
-    return availableReservationObjects.when(
-      data: (snapshot) => Stack(
-        children: <Widget>[
-          SingleChildScrollView(
-            key: UniqueKey(),
-            scrollDirection: Axis.horizontal,
-            child: VerticalReservationScrollViewWithStickyHeader(
-              boatsController: boatsController,
-              date: date,
-              snapshot: snapshot,
+    return filters.isEmpty
+        ? <Widget>[
+            const Icon(Icons.waves, color: Colors.blueGrey)
+                .padding(all: iconPadding),
+            const Text(
+              'Selecteer een categorie om te beginnen',
+              style: TextStyle(color: Colors.blueGrey),
             ),
-          ), // this builds the columns with the boats and the slots
-          TimeScrollView(
-            timesController: timesController,
-          ), // this builds the time column on the left side
-        ],
-      ),
-      loading: () => const Center(
-        child: CircularProgressIndicator(),
-      ),
-      error: (error, stk) => <Widget>[
-        const ErrorCardWidget(
-          errorMessage: "Er is iets misgegaan met het ophalen van de data",
-        ),
-      ].toColumn(mainAxisAlignment: MainAxisAlignment.center),
-    );
+          ].toColumn(mainAxisAlignment: MainAxisAlignment.center)
+        : Stack(
+            children: <Widget>[
+              SingleChildScrollView(
+                key: UniqueKey(),
+                scrollDirection: Axis.horizontal,
+                child: VerticalReservationScrollViewWithStickyHeader(
+                  boatsController: boatsController,
+                  date: widget.date,
+                ),
+              ), // this builds the columns with the boats and the slots
+              TimeScrollView(
+                timesController: timesController,
+              ), // this builds the time column on the left side
+            ],
+          );
   }
 }
--- lib/src/features/training/widgets/calendar/widgets/vertical_reservation_scrollview_with_sticky_header.dart
+++ lib/src/features/training/widgets/calendar/widgets/vertical_reservation_scrollview_with_sticky_header.dart
@@ -1,51 +1,59 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservation_object.dart';
+import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/filters/api/reservation_objects.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/object_calendar.dart';
 import 'package:ksrvnjord_main_app/src/features/training/widgets/calendar/widgets/reservation_object_name_box.dart';
 import 'package:sticky_headers/sticky_headers.dart';
 import 'package:styled_widget/styled_widget.dart';
 
-class VerticalReservationScrollViewWithStickyHeader extends StatelessWidget {
+class VerticalReservationScrollViewWithStickyHeader extends ConsumerWidget {
   const VerticalReservationScrollViewWithStickyHeader({
     super.key,
     required this.boatsController,
     required this.date,
-    required this.snapshot,
   });
 
   final ScrollController boatsController;
   final DateTime date;
-  final QuerySnapshot<ReservationObject> snapshot;
 
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      controller: boatsController,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 64),
-        child: StickyHeader(
-          header: snapshot.docs // this builds the header with the boat names
-              .map<Widget>(
-                (doc) => ReservationObjectNameBox(reservationObj: doc),
-              )
-              .toList()
-              .toRow(),
-          content: snapshot.docs // this builds the content with the slots
-              .map<Widget>((e) {
-                return ObjectCalendar(date: date, boat: e).border(
-                  left: 1,
-                  color: const Color.fromARGB(255, 223, 223, 223),
-                );
-              })
-              .toList()
-              .toRow(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-              ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final availableReservationObjects =
+        ref.watch(availableReservationObjectsProvider);
+
+    return availableReservationObjects.when(
+      data: (data) => SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        controller: boatsController,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 64),
+          child: StickyHeader(
+            header: data.docs // this builds the header with the boat names
+                .map<Widget>(
+                  (doc) => ReservationObjectNameBox(reservationObj: doc),
+                )
+                .toList()
+                .toRow(),
+            content: data.docs // this builds the content with the slots
+                .map<Widget>((e) {
+                  return ObjectCalendar(date: date, boat: e).border(
+                    left: 1,
+                    color: const Color.fromARGB(255, 223, 223, 223),
+                  );
+                })
+                .toList()
+                .toRow(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+          ),
         ),
       ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stack) => ErrorCardWidget(errorMessage: error.toString()),
     );
   }
 }
