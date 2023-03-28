412eacb0a9a8220a0443535189959c6152523335
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-28 23:12:25 +0200

    nice formatting of poll

--- lib/src/features/polls/pages/polls_page.dart
+++ lib/src/features/polls/pages/polls_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:ksrvnjord_main_app/src/features/polls/api/polls_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class PollsPage extends ConsumerWidget {
   const PollsPage({Key? key}) : super(key: key);
@@ -10,6 +12,8 @@ class PollsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final pollsVal = ref.watch(pollsProvider);
+    // create DateFormat for Dayname Day Month Year Time
+    final DateFormat dateFormat = DateFormat('EEEE d MMMM y HH:mm', 'nl_NL');
 
     return Scaffold(
       appBar: AppBar(
@@ -21,13 +25,34 @@ class PollsPage extends ConsumerWidget {
       ),
       body: pollsVal.when(
         data: (polls) => ListView.builder(
+          padding: const EdgeInsets.all(8),
           itemCount: polls.size,
           itemBuilder: (context, index) {
             final poll = polls.docs.toList()[index].data();
 
-            return ListTile(
-              title: Text(poll.question),
-            );
+            return [
+              ListTile(
+                title: Text(poll.question),
+                subtitle: Text(
+                  'Sluit op ${dateFormat.format(poll.openUntil)}',
+                ),
+              ),
+              ...poll.options.map((option) => RadioListTile(
+                    value: option,
+                    title: Text(option),
+                    onChanged: (_) => {},
+                    groupValue: 'Ja',
+                  )),
+            ].toColumn().card(
+                  color: Colors.white,
+                  elevation: 0,
+                  // add lightblue border
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.lightBlue, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  ),
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                );
           },
         ),
         loading: () => const Center(child: CircularProgressIndicator()),
