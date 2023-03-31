55dfcc2a5576546fc12ccf84e18e84496bcb826d
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-03-31 22:40:29 +0200

    slight refactor of vaarverbod to use riverpod

---
+++ lib/src/features/dashboard/api/vaarverbod_provider.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/dashboard/model/vaarverbod.dart';
+
+final vaarverbodProvider = FutureProvider<Vaarverbod>(
+  (ref) async {
+    final Response<Map<String, dynamic>> response =
+        await Dio().get('https://heimdall.njord.nl/api/v1/vaarverbod/');
+
+    return Vaarverbod.fromJson(response.data!);
+  },
+);
