ff44d7accb18dcaa3c46ba9ff448135a684cb82e
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-01 20:55:04 +0200

    x

--- lib/src/features/shared/model/cached_profile_picture.dart
+++ lib/src/features/shared/model/cached_profile_picture.dart
@@ -1,72 +1,33 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/model/cached_image.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/hive_cache.dart';
 
 class CachedProfilePicture {
-  static const String imagePathOnNoData = Images.placeholderProfilePicture;
-  static const String keyPrefix = "profile-avatar";
+  static const String placeholderImagePath = Images.placeholderProfilePicture;
   static const Duration maxAge = Duration(days: 7); // 1 week
 
-  static Future<ImageProvider<Object>> get(String lidnummer) async {
-    final key = "$keyPrefix-$lidnummer";
-    ImageProvider<Object>? image = await HiveCache.getHiveCachedImage(
-      key,
-      imageOnCacheHitWithNoData: Image.asset(imagePathOnNoData).image,
-    );
-    if (image != null) {
-      return image;
-    }
+  static Future<ImageProvider<Object>> get(String lidnummer) => CachedImage.get(
+        placeholderImagePath: placeholderImagePath,
+        firebaseStoragePath: path(lidnummer),
+        maxAge: maxAge,
+      );
 
-    String? url = await getProfilePictureUrl(
-      lidnummer,
-    );
-    if (url == null) {
-      HiveCache.putEmpty(key);
+  // path is the path to the image in firebase storage
+  static String path(String userId) => "$userId/profile_picture.png";
 
-      return Image.asset(imagePathOnNoData).image;
-    }
-
-    Uint8List? firestoreImage = await HiveCache.getHttpImageAndCache(
-      url,
-      key: keyPrefix,
-      maxAge: maxAge,
-    );
-    if (firestoreImage == null) {
-      return Image.asset(imagePathOnNoData).image;
-    }
-
-    return MemoryImage(firestoreImage);
-  }
-
-  static String profilePicturePath(String userId) =>
-      "$userId/profile_picture.png";
-
-  static Future<String?> getProfilePictureUrl(String userId) async {
-    try {
-      return FirebaseStorage.instance
-          .ref(profilePicturePath(userId))
-          .getDownloadURL();
-    } catch (e) {
-      return null;
-    }
-  }
-
-  static Future<Uint8List?> getProfilePicture(String userId) =>
-      FirebaseStorage.instance.ref(profilePicturePath(userId)).getData();
-
-  // Make function to getMyProfilePicture
-  static Future<Uint8List?> getMyProfilePicture() =>
-      getProfilePicture(FirebaseAuth.instance.currentUser!.uid);
+  static Future<ImageProvider<Object>> getMyProfilePicture() =>
+      get(FirebaseAuth.instance.currentUser!.uid);
 
   static UploadTask uploadMyProfilePicture(File file) {
     final String uid = FirebaseAuth.instance.currentUser!.uid;
-    HiveCache.delete("$keyPrefix-$uid"); // invalidate cache
+    String myPath = path(uid);
+    HiveCache.delete(myPath); // invalidate cache for my profile picture
 
-    return FirebaseStorage.instance.ref(profilePicturePath(uid)).putFile(file);
+    return FirebaseStorage.instance.ref(myPath).putFile(file);
   }
 }
