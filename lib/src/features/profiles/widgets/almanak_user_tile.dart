ddd5bf8c90a80a22d58a0170d67eab7c17173c89
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-02 18:44:16 +0200

    use futureprovider for retrieving almanakprofile to improve performance of repeated requests

--- lib/src/features/profiles/widgets/almanak_user_tile.dart
+++ lib/src/features/profiles/widgets/almanak_user_tile.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/models/profile.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/api/heimdall_almanak_profile.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/default_profile_picture.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/widgets/profile_picture_widget.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/shimmer_widget.dart';
-import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:styled_widget/styled_widget.dart';
 
-class AlmanakUserTile extends StatelessWidget {
+class AlmanakUserTile extends ConsumerWidget {
   const AlmanakUserTile({
     Key? key,
     required this.firstName,
@@ -23,24 +22,27 @@ class AlmanakUserTile extends StatelessWidget {
   final String lidnummer;
 
   @override
-  Widget build(BuildContext context) {
-    final client = Provider.of<GraphQLModel>(context).client;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final heimdallAlmanakProfile =
+        ref.watch(heimdallAlmanakProfileProvider(lidnummer));
 
     const double titleShimmerHeight = 18;
     const double subtitleShimmerHeight = 12;
     const double titleShimmerPadding = 128;
     const double subtitleShimmerPadding = 64;
 
-    return FutureWrapper(
-      future: almanakProfileByIdentifier(lidnummer, client),
-      success: (user) => ListTile(
-        leading: ProfilePictureWidget(userId: lidnummer),
-        title: Text("$firstName $lastName"),
-        subtitle: subtitle != null ? Text(subtitle!) : null,
-        trailing: const Icon(Icons.arrow_forward_ios, color: Colors.lightBlue),
-        onTap: () => Routemaster.of(context).push(user!.identifier),
-      ),
-      loading: ListTile(
+    return heimdallAlmanakProfile.when(
+      data: (user) => user == null
+          ? const SizedBox() // no user found, show nothing
+          : ListTile(
+              leading: ProfilePictureWidget(userId: lidnummer),
+              title: Text("$firstName $lastName"),
+              subtitle: subtitle != null ? Text(subtitle!) : null,
+              trailing:
+                  const Icon(Icons.arrow_forward_ios, color: Colors.lightBlue),
+              onTap: () => Routemaster.of(context).push(user.identifier),
+            ),
+      loading: () => ListTile(
         leading: const ShimmerWidget(child: DefaultProfilePicture()),
         title: ShimmerWidget(
           child: Container(
@@ -51,16 +53,20 @@ class AlmanakUserTile extends StatelessWidget {
             ),
           ),
         ).padding(right: titleShimmerPadding),
-        subtitle: ShimmerWidget(
-          child: Container(
-            height: subtitleShimmerHeight,
-            decoration: ShapeDecoration(
-              shape: const RoundedRectangleBorder(),
-              color: Colors.grey[300],
-            ),
-          ),
-        ).padding(right: subtitleShimmerPadding),
+        subtitle: subtitle == null
+            ? null
+            : ShimmerWidget(
+                child: Container(
+                  height: subtitleShimmerHeight,
+                  decoration: ShapeDecoration(
+                    shape: const RoundedRectangleBorder(),
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ).padding(right: subtitleShimmerPadding),
       ),
+      error: (error, stackTrace) =>
+          ErrorCardWidget(errorMessage: error.toString()),
     ); // show nothing if no heimdall user is found
   }
 }
