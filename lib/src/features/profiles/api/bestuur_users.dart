2079a15c614a9d256844c8d67be58d68dd5012ee
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-02 21:36:47 +0200

    bestuur also use provider

---
+++ lib/src/features/profiles/api/bestuur_users.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/models/almanak_profile.dart';
+
+final peopleRef = FirebaseFirestore.instance.collection("people").withConverter(
+      fromFirestore: (snapshot, _) => AlmanakProfile.fromJson(snapshot.data()!),
+      toFirestore: (almanakProfile, _) => almanakProfile.toJson(),
+    );
+
+final bestuurUsersProvider = FutureProvider<QuerySnapshot<AlmanakProfile>>(
+  (ref) => peopleRef.where("bestuurs_functie", isNull: false).get(),
+);
