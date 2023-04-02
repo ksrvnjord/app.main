342c9726ea8d3c27761ece2b7e940f3133998abe
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-02 20:52:58 +0200

    use futureprovider for getting heimdalldata

--- lib/src/features/profiles/api/user_profile.dart
+++ lib/src/features/profiles/api/user_profile.dart
@@ -1,27 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/firestore_user.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/api/profile.graphql.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_by_identifier.graphql.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/models/almanak_profile.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/models/profile.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
 
 // retrieves all data from firestore and heimdall for a given user
 final almanakUserProvider =
-    FutureProvider.family<AlmanakProfile, String>((ref, identifier) async {
-  final client = ref.watch(graphQLModelProvider).client;
+    FutureProvider.family<AlmanakProfile, String>((ref, lidnummer) async {
   if (FirebaseAuth.instance.currentUser == null) {
-    // if in DEMO mode
-    return AlmanakProfile.fromHeimdall(
-      (await almanakProfile(identifier, client))!,
-    );
+    // if in DEMO mode, the lidnummer is the heimdall id
+    final profile = await ref.watch(heimdallUserByIdProvider(lidnummer).future);
+
+    return AlmanakProfile.fromHeimdall(profile!);
   }
 
   // call both queries in parallel
-  Future<Query$AlmanakProfileByIdentifier$userByIdentifier?> heimdallProfile =
-      almanakProfileByIdentifier(identifier, client);
+  final heimdallProfile =
+      ref.watch(heimdallUserByLidnummerProvider(lidnummer).future);
 
-  AlmanakProfile? profile = await getFirestoreProfileData(identifier);
+  AlmanakProfile? profile = await getFirestoreProfileData(lidnummer);
   final heimdallProfileData = await heimdallProfile;
 
   // merge the data
@@ -32,3 +32,35 @@ final almanakUserProvider =
         heimdallProfileData!,
       ); // if no profile was found in firestore, return the heimdall profile
 });
+
+final heimdallUserByLidnummerProvider = FutureProvider.family<
+    Query$AlmanakProfileByIdentifier$userByIdentifier?,
+    String>((ref, identifier) async {
+  final client = ref.watch(graphQLModelProvider).client;
+
+  final result = await client.query$AlmanakProfileByIdentifier(
+    Options$Query$AlmanakProfileByIdentifier(
+      variables:
+          Variables$Query$AlmanakProfileByIdentifier(profileId: identifier),
+      fetchPolicy: FetchPolicy.cacheFirst,
+    ),
+  );
+
+  return result.parsedData?.userByIdentifier;
+});
+
+final heimdallUserByIdProvider =
+    FutureProvider.family<Query$AlmanakProfile$user?, String>(
+  (ref, heimdallId) async {
+    final client = ref.watch(graphQLModelProvider).client;
+
+    final result = await client.query$AlmanakProfile(
+      Options$Query$AlmanakProfile(
+        variables: Variables$Query$AlmanakProfile(profileId: heimdallId),
+        fetchPolicy: FetchPolicy.cacheFirst,
+      ),
+    );
+
+    return result.parsedData?.user;
+  },
+);
