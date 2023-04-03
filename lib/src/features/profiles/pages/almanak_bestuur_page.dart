8d1750143280c20afb27c0288ad4107cb6e12af8
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-03 12:04:12 +0200

    don't make the small profile pictures zoomable

--- lib/src/features/profiles/pages/almanak_bestuur_page.dart
+++ lib/src/features/profiles/pages/almanak_bestuur_page.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
