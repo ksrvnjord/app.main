1ba1dadbb436422764237b4c18a896ba5bbd796b
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-03 16:28:23 +0200

    we probably shouldn't preserve state for images, considering the memory usage

--- lib/src/features/profiles/api/substructure_picture_provider.dart
+++ lib/src/features/profiles/api/substructure_picture_provider.dart
@@ -4,8 +4,8 @@ import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/cached_image.dart';
 import 'package:tuple/tuple.dart';
 
-final commissiePictureProvider =
-    FutureProvider.family<ImageProvider<Object>, Tuple2<String, int>>(
+final commissiePictureProvider = FutureProvider.autoDispose
+    .family<ImageProvider<Object>, Tuple2<String, int>>(
   (ref, commissieAndYear) {
     final commissie = commissieAndYear.item1;
     final year = commissieAndYear.item2;
