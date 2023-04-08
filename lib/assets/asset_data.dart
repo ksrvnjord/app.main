f87136f73699f17f59a990a11bfe9a3fe6e22372
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-08 11:31:49 +0200

    add commissies description data

--- lib/assets/asset_data.dart
+++ lib/assets/asset_data.dart
@@ -3,4 +3,5 @@ import 'package:flutter/foundation.dart';
 @immutable
 class AssetData {
   static const String substructuren = 'assets/data/substructuren.yaml';
+  static const String commissies = 'assets/data/commissies.yaml';
 }
