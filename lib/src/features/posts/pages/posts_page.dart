2b8389e91f64d86378acf24ef9e764ddc94e8575
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-08 17:08:52 +0200

    refactor

--- lib/src/features/posts/pages/posts_page.dart
+++ lib/src/features/posts/pages/posts_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/api/post_topics_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/api/posts_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_list.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:routemaster/routemaster.dart';
@@ -14,59 +13,55 @@ class PostsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ref.watch(postTopicsProvider).when(
-          data: (topics) => DefaultTabController(
-            length: topics.length,
-            animationDuration: const Duration(
-              milliseconds: 1726 ~/ 2,
-            ), // no need to explain this
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text("Prikbord"),
-                backgroundColor: Colors.lightBlue,
-                shadowColor: Colors.transparent,
-                systemOverlayStyle: const SystemUiOverlayStyle(
-                  statusBarColor: Colors.lightBlue,
-                ),
-                bottom: TabBar(
-                  isScrollable: true,
-                  labelColor: Colors.white,
-                  labelStyle: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+    return Scaffold(
+      // we need this extra top-level scaffold, in case of loading/error
+      body: ref.watch(postTopicsProvider).when(
+            data: (topics) => DefaultTabController(
+              length: topics.length,
+              animationDuration: const Duration(
+                milliseconds: 1726 ~/ 2,
+              ), // no need to explain this
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text("Prikbord"),
+                  backgroundColor: Colors.lightBlue,
+                  shadowColor: Colors.transparent,
+                  systemOverlayStyle: const SystemUiOverlayStyle(
+                    statusBarColor: Colors.lightBlue,
                   ),
-                  unselectedLabelStyle: const TextStyle(fontSize: 12),
-                  unselectedLabelColor: Colors.white60,
-                  tabs: topics
-                      .map((topic) => Tab(
+                  bottom: TabBar(
+                    isScrollable: true,
+                    labelColor: Colors.white,
+                    labelStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    unselectedLabelStyle: const TextStyle(fontSize: 12),
+                    unselectedLabelColor: Colors.white60,
+                    tabs: [
+                      ...topics.map((topic) => Tab(
                             text: topic,
-                          ))
-                      .toList(),
+                          )),
+                    ],
+                  ),
                 ),
-              ),
-              floatingActionButton: FloatingActionButton.extended(
-                onPressed: () => Routemaster.of(context).push('new'),
-                backgroundColor: Colors.blue,
-                icon: const Icon(Icons.add),
-                label: const Text('Nieuw bericht'),
-              ),
-              body: TabBarView(
-                children: topics
-                    .map((topic) => ref.watch(postsProvider(topic)).when(
-                          data: (snapshot) => PostList(snapshot: snapshot),
-                          loading: () => const Center(
-                            child: CircularProgressIndicator(),
-                          ),
-                          error: (error, stack) =>
-                              ErrorCardWidget(errorMessage: error.toString()),
-                        ))
-                    .toList(),
+                floatingActionButton: FloatingActionButton.extended(
+                  onPressed: () => Routemaster.of(context).push('new'),
+                  backgroundColor: Colors.blue,
+                  icon: const Icon(Icons.add),
+                  label: const Text('Nieuw bericht'),
+                ),
+                body: TabBarView(
+                  children: [
+                    ...topics.map((topic) => PostList(topic: topic)),
+                  ],
+                ),
               ),
             ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stack) =>
+                ErrorCardWidget(errorMessage: error.toString()),
           ),
-          loading: () => const Center(child: CircularProgressIndicator()),
-          error: (error, stack) =>
-              ErrorCardWidget(errorMessage: error.toString()),
-        );
+    );
   }
 }
--- lib/src/features/posts/widgets/post_list.dart
+++ lib/src/features/posts/widgets/post_list.dart
@@ -1,19 +1,32 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/model/post.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/api/posts_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 
-class PostList extends StatelessWidget {
-  final QuerySnapshot<Post> snapshot;
-  const PostList({Key? key, required this.snapshot}) : super(key: key);
+class PostList extends ConsumerWidget {
+  const PostList({Key? key, required this.topic}) : super(key: key);
+
+  final String topic;
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: snapshot.size,
-      padding: const EdgeInsets.all(16),
-      separatorBuilder: (context, index) => const SizedBox(height: 16),
-      itemBuilder: ((context, index) => PostWidget(doc: snapshot.docs[index])),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final posts = ref.watch(postsProvider(topic));
+
+    return posts.when(
+      data: (snapshot) => ListView.separated(
+        itemCount: snapshot.size,
+        padding: const EdgeInsets.all(16),
+        separatorBuilder: (context, index) => const SizedBox(height: 16),
+        itemBuilder: ((context, index) =>
+            PostWidget(doc: snapshot.docs[index])),
+      ),
+      loading: () => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, stack) => ErrorCardWidget(
+        errorMessage: error.toString(),
+      ),
     );
   }
 }
