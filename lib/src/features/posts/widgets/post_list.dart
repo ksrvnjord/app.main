ec4fe3d1de793c2d85a738ee375bbe3bb18f1324
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-08 16:35:21 +0200

    refactor the card of a post to a separate widget

--- lib/src/features/posts/widgets/post_list.dart
+++ lib/src/features/posts/widgets/post_list.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:expand_widget/expand_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/model/post.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/widgets/comment_list.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/widgets/display_likes.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_widget.dart';
 
 class PostList extends StatelessWidget {
   final QuerySnapshot<Post> snapshot;
@@ -11,42 +9,15 @@ class PostList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: snapshot.size,
+      padding: const EdgeInsets.all(16),
+      separatorBuilder: (context, index) => const SizedBox(height: 16),
       itemBuilder: ((context, index) {
         final doc = snapshot.docs[index];
         final post = doc.data();
 
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-          elevation: 1,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16)),
-          ),
-          child: Column(children: [
-            Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Icon(
-                Icons.person,
-                size: 40,
-              ),
-              const SizedBox(width: 20),
-              Text(
-                post.title,
-                style:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-              DisplayLikes(
-                docRef: doc.reference,
-                likedBy: post.likedBy,
-                iconSize: 20,
-              ),
-            ]),
-            Text(post.content),
-            const Divider(),
-            ExpandChild(child: CommentList(post: doc)),
-          ]),
-        );
+        return PostWidget(post: post, doc: doc);
       }),
     );
   }
---
+++ lib/src/features/posts/widgets/post_widget.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expand_widget/expand_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/model/post.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/widgets/comment_list.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/widgets/display_likes.dart';
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({
+    super.key,
+    required this.post,
+    required this.doc,
+  });
+
+  final Post post;
+  final QueryDocumentSnapshot<Post> doc;
+
+  static const likeIconSize = 20.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Column(children: [
+        Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Icon(
+            Icons.person,
+            size: 40,
+          ),
+          const SizedBox(width: 20),
+          Text(
+            post.title,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+          const Spacer(),
+          DisplayLikes(
+            docRef: doc.reference,
+            likedBy: post.likedBy,
+            iconSize: likeIconSize,
+          ),
+        ]),
+        Text(post.content),
+        const Divider(),
+        ExpandChild(child: CommentList(post: doc)),
+      ]),
+    );
+  }
+}
