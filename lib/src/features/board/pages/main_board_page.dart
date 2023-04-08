805157c53ea72501dcd5c7c5b0e1273e630ddcce
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-08 12:51:11 +0200

    use post topics provider to separate logic from ui

--- lib/src/features/board/pages/main_board_page.dart
+++ lib/src/features/board/pages/main_board_page.dart
@@ -1,18 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/board/api/post_topics_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/board/widgets/post_list.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:routemaster/routemaster.dart';
 
-class MainBoardPage extends StatelessWidget {
+class MainBoardPage extends ConsumerWidget {
   const MainBoardPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     Routemaster navigator = Routemaster.of(context);
     FirebaseFirestore db = FirebaseFirestore.instance;
+
+    final postTopics = ref.watch(postTopicsProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -28,65 +33,56 @@ class MainBoardPage extends StatelessWidget {
         icon: const Icon(Icons.add),
         label: const Text('Nieuw bericht'),
       ),
-      body: FutureBuilder<QuerySnapshot>(
-        future: db.collection('postTopics').get(),
-        builder: (
-          BuildContext context,
-          AsyncSnapshot<QuerySnapshot> snapshot,
-        ) {
-          if (!snapshot.hasData) {
-            return const CircularProgressIndicator();
-          }
-          QuerySnapshot<Object?>? data = snapshot.data;
-          List<String> topics = data!.docs.map((e) => e.id).toList();
-
-          return DefaultTabController(
-            length: data.size,
-            animationDuration: const Duration(milliseconds: 1726 ~/ 2),
-            child: Scaffold(
-              appBar: AppBar(
-                title: TabBar(
-                  tabs: topics
-                      .map((e) => Text(e, style: const TextStyle(fontSize: 25)))
-                      .toList(),
-                ),
-              ),
-              body: TabBarView(
-                children: topics
-                    .map(
-                      (e) => StreamBuilder<QuerySnapshot>(
-                        stream: db
-                            .collection('posts')
-                            .where('topic', isEqualTo: e)
-                            .where(
-                              'createdTime',
-                              isGreaterThan: DateTime.now()
-                                  .subtract(const Duration(days: 14)),
-                            )
-                            .orderBy('createdTime', descending: true)
-                            .snapshots(),
-                        builder: (
-                          BuildContext context,
-                          AsyncSnapshot<QuerySnapshot> snapshot,
-                        ) {
-                          if (snapshot.hasData) {
-                            return PostList(snapshot: snapshot);
-                          }
-                          if (snapshot.hasError) {
-                            return Text(snapshot.error.toString());
-                          } else {
-                            return const Text(
-                              'Geen recente activiteit op dit onderwerp',
-                            );
-                          }
-                        },
-                      ),
-                    )
+      body: postTopics.when(
+        data: (topics) => DefaultTabController(
+          length: topics.length,
+          animationDuration: const Duration(milliseconds: 1726 ~/ 2),
+          child: Scaffold(
+            appBar: AppBar(
+              title: TabBar(
+                tabs: topics
+                    .map((e) => Text(e, style: const TextStyle(fontSize: 25)))
                     .toList(),
               ),
             ),
-          );
-        },
+            body: TabBarView(
+              children: topics
+                  .map(
+                    (e) => StreamBuilder<QuerySnapshot>(
+                      stream: db
+                          .collection('posts')
+                          .where('topic', isEqualTo: e)
+                          .where(
+                            'createdTime',
+                            isGreaterThan: DateTime.now()
+                                .subtract(const Duration(days: 14)),
+                          )
+                          .orderBy('createdTime', descending: true)
+                          .snapshots(),
+                      builder: (
+                        BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot,
+                      ) {
+                        if (snapshot.hasData) {
+                          return PostList(snapshot: snapshot);
+                        }
+                        if (snapshot.hasError) {
+                          return Text(snapshot.error.toString());
+                        } else {
+                          return const Text(
+                            'Geen recente activiteit op dit onderwerp',
+                          );
+                        }
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stack) =>
+            ErrorCardWidget(errorMessage: error.toString()),
       ),
     );
   }
