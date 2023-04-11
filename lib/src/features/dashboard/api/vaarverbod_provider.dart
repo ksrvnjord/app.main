946a72ce10ee7bb1b7d8ac0cfd0d6ae2df88de3e
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-11 22:27:45 +0200

    better lifecycle of widget with updated error and loading display

--- lib/src/features/dashboard/api/vaarverbod_provider.dart
+++ lib/src/features/dashboard/api/vaarverbod_provider.dart
@@ -2,10 +2,13 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/dashboard/model/vaarverbod.dart';
 
-final vaarverbodProvider = FutureProvider<Vaarverbod>(
+final vaarverbodProvider = FutureProvider.autoDispose<Vaarverbod>(
   (ref) async {
     final Response<Map<String, dynamic>> response =
         await Dio().get('https://heimdall.njord.nl/api/v1/vaarverbod/');
+    await Future.delayed(
+      const Duration(milliseconds: 1726 ~/ 2),
+    ); // add delay so people believe it's loading
 
     return Vaarverbod.fromJson(response.data!);
   },
