8aa1dd4e9cef7058082775657ec030ac25302f25
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-12 12:52:03 +0200

    remove unused imports

--- lib/src/features/training/pages/training_page.dart
+++ lib/src/features/training/pages/training_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/firebase_widget.dart';
