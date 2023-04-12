b45bcf89e9a0bcbb6bc719e5997a3bd9a53b3f55
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-12 17:24:55 +0200

    add 5 topics

--- lib/src/features/posts/api/post_topics_provider.dart
+++ lib/src/features/posts/api/post_topics_provider.dart
@@ -1,10 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final postTopicsProvider = FutureProvider<List<String>>((ref) async {
-  final snapshot =
-      await FirebaseFirestore.instance.collection('postTopics').get();
-
+final postTopicsProvider = Provider<List<String>>((ref) {
   // return the 'name' field of each document
-  return snapshot.docs.map((e) => e.data()['name'] as String).toList();
+  return [
+    'Promotie',
+    'Wandelgangen',
+    'Coach gezocht',
+    'Gevonden voorwerpen',
+    'Kaartjes',
+  ];
 });
--- lib/src/features/posts/model/topic.dart
+++
@@ -1,7 +0,0 @@
-enum Topic {
-  wandelGangen("Wandelgangen");
-
-  const Topic(this.name);
-
-  final String name;
-}
