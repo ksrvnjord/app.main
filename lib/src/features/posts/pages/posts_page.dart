f82e4352ae40dde29740bf29a1535c1edb8e585e
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-13 15:08:06 +0200

    creating post for topic you're currently in

--- lib/src/features/posts/pages/posts_page.dart
+++ lib/src/features/posts/pages/posts_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/api/post_topics_provider.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/api/selected_topic_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_list.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/api/firebase_currentuser_provider.dart';
 import 'package:routemaster/routemaster.dart';
@@ -38,42 +39,51 @@ class PostsPage extends ConsumerWidget {
               animationDuration: const Duration(
                 milliseconds: 1726 ~/ 2,
               ), // no need to explain this
-              child: Scaffold(
-                appBar: AppBar(
-                  title: const Text("Prikbord"),
-                  backgroundColor: Colors.lightBlue,
-                  shadowColor: Colors.transparent,
-                  systemOverlayStyle: const SystemUiOverlayStyle(
-                    statusBarColor: Colors.lightBlue,
-                  ),
-                  bottom: TabBar(
-                    isScrollable: true,
-                    labelColor: Colors.white,
-                    labelStyle: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+              child: Builder(builder: (context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Prikbord"),
+                    backgroundColor: Colors.lightBlue,
+                    shadowColor: Colors.transparent,
+                    systemOverlayStyle: const SystemUiOverlayStyle(
+                      statusBarColor: Colors.lightBlue,
                     ),
-                    unselectedLabelStyle: const TextStyle(fontSize: 12),
-                    unselectedLabelColor: Colors.white60,
-                    tabs: [
-                      ...topics.map((topic) => Tab(
-                            text: topic,
-                          )),
+                    bottom: TabBar(
+                      isScrollable: true,
+                      labelColor: Colors.white,
+                      labelStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      unselectedLabelStyle: const TextStyle(fontSize: 12),
+                      unselectedLabelColor: Colors.white60,
+                      tabs: [
+                        ...topics.map((topic) => Tab(
+                              text: topic,
+                            )),
+                      ],
+                    ),
+                  ),
+                  floatingActionButton: FloatingActionButton.extended(
+                    // small function so we can use the ignore comment
+                    // ignore: prefer-extracting-callbacks
+                    onPressed: () {
+                      ref.read(selectedTopicProvider.notifier).state =
+                          topics[DefaultTabController.of(context).index];
+                      Routemaster.of(context).push('new');
+                    },
+                    backgroundColor: Colors.blue,
+                    icon: const Icon(Icons.add),
+                    label: const Text('Nieuw bericht'),
+                  ),
+                  body: TabBarView(
+                    // TODO: get index: https://stackoverflow.com/a/54606911
+                    children: [
+                      ...topics.map((topic) => PostList(topic: topic)),
                     ],
                   ),
-                ),
-                floatingActionButton: FloatingActionButton.extended(
-                  onPressed: () => Routemaster.of(context).push('new'),
-                  backgroundColor: Colors.blue,
-                  icon: const Icon(Icons.add),
-                  label: const Text('Nieuw bericht'),
-                ),
-                body: TabBarView(
-                  children: [
-                    ...topics.map((topic) => PostList(topic: topic)),
-                  ],
-                ),
-              ),
+                );
+              }),
             ),
           );
   }
