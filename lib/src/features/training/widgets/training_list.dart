947c6e2aa55d45a4280858510a07f760787b3486
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-29 20:50:31 +0200

    refactor for cleanness

--- lib/src/features/training/widgets/training_list.dart
+++ lib/src/features/training/widgets/training_list.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/api/firebase_currentuser_provider.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/stream_wrapper.dart';
 import 'package:ksrvnjord_main_app/src/features/training/api/my_reservations_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/training/model/reservation.dart';
@@ -13,35 +15,33 @@ class TrainingList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    if (FirebaseAuth.instance.currentUser == null) {
+    if (ref.watch(firebaseAuthUserProvider) == null) {
       // this should show to the user that there are no reservations
       return const Center(
         child: Text('Je hebt geen afschrijvingen'),
       );
     }
 
-    return StreamWrapper(
-      stream: ref.watch(myReservationsProvider.stream),
-      success: showMyReservations,
-    );
-  }
-
-  Widget showMyReservations(QuerySnapshot<Reservation> snapshot) {
-    if (snapshot.docs.isEmpty) {
-      return Center(
-        // ignore: avoid-non-ascii-symbols
-        child: const Text('Het is wel leeg hier...').textColor(Colors.blueGrey),
-      );
-    }
-
-    return ListView.separated(
-      itemCount: snapshot.docs.length,
-      padding: const EdgeInsets.all(10),
-      separatorBuilder: (BuildContext context, int index) =>
-          const SizedBox(height: 4),
-      itemBuilder: (BuildContext context, int index) => Center(
-        child: TrainingListItem(reservation: snapshot.docs[index]),
-      ),
-    );
+    return ref.watch(myReservationsProvider).when(
+          error: (err, stk) => ErrorCardWidget(errorMessage: err.toString()),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          data: (data) => data.docs.isEmpty
+              ? Center(
+                  // ignore: avoid-non-ascii-symbols
+                  child: const Text('Je hebt geen afschrijvingen op dit moment')
+                      .textColor(Colors.blueGrey),
+                )
+              : ListView.separated(
+                  itemCount: data.docs.length,
+                  padding: const EdgeInsets.all(10),
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 4),
+                  itemBuilder: (BuildContext context, int index) => Center(
+                    child: TrainingListItem(reservation: data.docs[index]),
+                  ),
+                ),
+        );
   }
 }
