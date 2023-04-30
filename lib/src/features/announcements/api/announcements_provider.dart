f4476449f01da58d9828300b12234fe39da428c1
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-04-30 11:11:15 +0200

    simplify announcements architecture

---
+++ lib/src/features/announcements/api/announcements_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/announcements/api/announcements.graphql.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
+
+// get first page of announcements
+final announcementsProvider =
+    FutureProvider<List<Query$Announcements$announcements$data>>((ref) async {
+  final client = ref.watch(graphQLModelProvider).client;
+  final result = await client.query$Announcements(Options$Query$Announcements(
+    variables: Variables$Query$Announcements(
+      page: 0,
+    ),
+  ));
+
+  return result.parsedData!.announcements!.data;
+});
--- lib/src/features/dashboard/widgets/announcements_widget.dart
+++ lib/src/features/dashboard/widgets/announcements_widget.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:ksrvnjord_main_app/src/features/announcements/api/announcements.graphql.dart';
-import 'package:ksrvnjord_main_app/src/features/announcements/models/announcements.dart';
-import 'package:ksrvnjord_main_app/src/features/announcements/widgets/announcement_list_widget.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/model/graphql_model.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/future_wrapper.dart';
+import 'package:ksrvnjord_main_app/src/features/announcements/api/announcements_provider.dart';
+import 'package:ksrvnjord_main_app/src/features/announcements/widgets/announcement_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/dashboard/widgets/widget_header.dart';
+import 'package:routemaster/routemaster.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class AnnouncementsWidget extends ConsumerWidget {
   const AnnouncementsWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final client = ref.watch(graphQLModelProvider).client;
+    final announcementsVal = ref.watch(announcementsProvider);
 
-    return FutureWrapper<Query$Announcements?>(
-      future: announcements(client),
-      success: showAnnouncementsList,
-    );
-  }
-
-  Widget showAnnouncementsList(data) {
-    return AnnouncementListWidget(
-      announcements: data!.announcements!.data,
+    return announcementsVal.when(
+      data: (announcements) => [
+        const WidgetHeader(title: "Recente aankondigingen"),
+        announcements
+            .map<Widget>(
+              (announcement) => InkWell(
+                child: AnnouncementWidget(
+                  title: announcement.title,
+                  subtitle:
+                      "${announcement.author} - ${DateFormat.yMMMd().format(announcement.created_at)}",
+                  text: '',
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 2,
+                  ),
+                ),
+                onTap: () => Routemaster.of(context)
+                    .push('announcements/${announcement.id}'),
+              ),
+            )
+            .toList()
+            .toColumn(crossAxisAlignment: CrossAxisAlignment.stretch),
+      ].toColumn(),
+      error: (error, stackTrace) => Text(error.toString()),
+      loading: () => const CircularProgressIndicator(),
     );
   }
 }
