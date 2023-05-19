e7665fa0b5f154542e5192219060e880afccbf54
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-19 23:39:48 +0200

    add analytic for creating reservation

--- lib/src/features/training/pages/plan_training_page.dart
+++ lib/src/features/training/pages/plan_training_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -313,6 +314,12 @@ class _PlanTrainingPageState extends ConsumerState<PlanTrainingPage> {
       creatorName: creatorName,
     )).then((res) {
       if (res['success'] == true) {
+        FirebaseAnalytics.instance.logEvent(
+          name: 'reservation_created',
+          parameters: <String, dynamic>{
+            'reservation_object_name': widget.objectName,
+          },
+        );
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
             showCloseIcon: true,
