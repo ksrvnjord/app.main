3d2e0807e6dd53d9bf4adb8580f749bf008eff86
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-26 00:05:17 +0200

    add back button to gallery

--- lib/src/features/gallery/pages/gallery_folder_page.dart
+++ lib/src/features/gallery/pages/gallery_folder_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/gallery/api/gallery_storage.dart';
 import 'package:ksrvnjord_main_app/src/features/gallery/widgets/folder_list.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/loading_widget.dart';
+import 'package:routemaster/routemaster.dart';
 
 class GalleryFolderPage extends ConsumerWidget {
   final String path;
@@ -19,6 +20,13 @@ class GalleryFolderPage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
+        leading: path ==
+                '/' // Lousy fix for having a Navigator in the GalleryMainPage so we can navigate back out of the gallery
+            ? IconButton(
+                onPressed: () => Routemaster.of(context).pop(),
+                icon: const Icon(Icons.arrow_back),
+              )
+            : null,
         title: Text(path == '/' ? "Galerij" : path.replaceAll('-', ' ')),
         backgroundColor: Colors.lightBlue,
         shadowColor: Colors.transparent,
