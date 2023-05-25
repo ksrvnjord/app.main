916c4b2b208e63f066c2e422743a0fa6b78d0e91
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-25 22:57:58 +0200

    clarify what happens in the thumbnail function

--- lib/src/features/gallery/utils/get_thumbnail_reference.dart
+++ lib/src/features/gallery/utils/get_thumbnail_reference.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
-const secondToLast = 1;
-
 Reference getThumbnailReference(Reference ref) {
   if (ref.parent == null) {
     throw Exception(
@@ -9,15 +7,25 @@ Reference getThumbnailReference(Reference ref) {
     );
   }
 
-  // Move the file to the thumbnail folder
+  // Firebase Extensions makes thumbnails in the Thumbnails folder
   List<String> pathComponents = ref.fullPath.split('/');
   pathComponents.insert(
-    pathComponents.length - secondToLast,
+    //Insert path to thumbnails folder
+    // Example: /galerij/2022 duinloop/image.jpg -> /galerij/2022 duinloop/thumbnails/image.jpg
+    pathComponents.length - 1,
     'thumbnails',
   );
 
+  // We want to serve the 200x200 thumbnail, so we replace the first dot with _200x200.
+  // find the last dot position
+  //// Example: image.jpg -> image_200x200.jpg
+  pathComponents.last = pathComponents.last.replaceFirst(
+    '.',
+    '_200x200.',
+    pathComponents.last.lastIndexOf('.'),
+  );
+
   String fullPath = pathComponents.join('/');
-  fullPath = fullPath.replaceAll('.', '_200x200.');
 
   return ref.root.child(fullPath);
 }
