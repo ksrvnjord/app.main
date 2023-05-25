693a838ebae9c739c36f676c76c023e0b2443dc5
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-26 00:05:23 +0200

    remove unused imports

--- lib/src/features/gallery/pages/gallery_file_page.dart
+++ lib/src/features/gallery/pages/gallery_file_page.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/gallery/api/gallery_image_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/gallery/api/gallery_storage.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/loading_widget.dart';
 import 'package:share_plus/share_plus.dart';
