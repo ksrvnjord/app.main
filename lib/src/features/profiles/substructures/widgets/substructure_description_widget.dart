adfcda27f598df786114ac595e298be7c08f5783
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-31 23:52:16 +0200

    make loading animation of description use shimmer effect

--- lib/src/features/profiles/substructures/widgets/substructure_description_widget.dart
+++ lib/src/features/profiles/substructures/widgets/substructure_description_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/widgets/shimmer_widget.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class SubstructureDescriptionWidget extends ConsumerWidget {
@@ -13,6 +14,8 @@ class SubstructureDescriptionWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    const double loadingHeight = 120;
+
     return descriptionAsyncVal.when(
       data: (data) => data == null
           ? const SizedBox.shrink()
@@ -22,7 +25,14 @@ class SubstructureDescriptionWidget extends ConsumerWidget {
                 style: Theme.of(context).textTheme.bodyMedium,
               ).expanded(flex: 0),
             ].toColumn(),
-      loading: () => const CircularProgressIndicator().center(),
+      loading: () => ShimmerWidget(
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: const BorderRadius.all(Radius.circular(12))),
+          height: loadingHeight,
+        ),
+      ),
       error: (error, stack) => ErrorCardWidget(
         errorMessage: error.toString(),
       ),
