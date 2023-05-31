780d2801c800e6cfef64f0b6e5531fc6d5e34a6e
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-05-31 23:35:00 +0200

    add thumbnails to choice page for homes

---
+++ lib/src/features/profiles/choice/huis_choice_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/api/njord_year.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/substructures/api/huis_picture_provider.dart';
+import 'package:ksrvnjord_main_app/src/features/profiles/widgets/substructure_choice_list_tile.dart';
+import 'package:styled_widget/styled_widget.dart';
+import 'package:tuple/tuple.dart';
+
+/// Page that shows a list of choices, and pushes a new page when a choice is chosen.
+class HuisChoicePage extends ConsumerWidget {
+  const HuisChoicePage({
+    Key? key,
+    required this.title,
+    required this.choices,
+  }) : super(key: key);
+
+  final String title;
+  final List<String> choices;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) => [
+          SubstructureChoiceListTile(
+            name: choices[index],
+            imageProvider: ref.watch(huisThumbnailProvider(
+              Tuple2(choices[index], getNjordYear()),
+            )),
+          ),
+          const Divider(height: 0, thickness: 0.5),
+        ].toColumn(),
+        itemCount: choices.length,
+      ),
+    );
+  }
+}
--- lib/src/features/profiles/substructures/api/huis_picture_provider.dart
+++ lib/src/features/profiles/substructures/api/huis_picture_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/model/cached_image.dart';
+import 'package:ksrvnjord_main_app/src/features/shared/model/thumbnail.dart';
 import 'package:tuple/tuple.dart';
 
 // ignore: prefer-static-class
@@ -13,6 +14,22 @@ final huisPictureProvider = FutureProvider.autoDispose
 
     return CachedImage.get(
       firebaseStoragePath: "almanak/huizen/$huis/$year/picture.jpg",
+      placeholderImagePath: Images.placeholderProfilePicture,
+      maxAge: const Duration(days: 14),
+    );
+  },
+);
+
+// ignore: prefer-static-class
+final huisThumbnailProvider = FutureProvider.autoDispose
+    .family<ImageProvider<Object>, Tuple2<String, int>>(
+  (ref, huisAndYear) {
+    final huis = huisAndYear.item1;
+    final year = huisAndYear.item2;
+
+    return CachedImage.get(
+      firebaseStoragePath:
+          "almanak/huizen/$huis/$year/thumbnails/picture${Thumbnail.x200}.jpg",
       placeholderImagePath: Images.placeholderProfilePicture,
       maxAge: const Duration(days: 14),
     );
