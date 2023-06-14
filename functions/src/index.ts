6ec9fdce3fb87d7da6fa39afc9e67f43b1b18eed
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-06-14 15:26:24 +0200

    substructures cron job

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ admin.initializeApp();
 
 export {createReservation} from "./create-reservation";
 export {monitorDamages} from "./monitor-damages";
+export {substructuresMigration} from "./substructures-migration";
 
 // Run this function if you want to test the client
 export const alwaysSuccesful = functions.region("europe-west1").https
