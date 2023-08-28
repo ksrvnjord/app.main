a696d698bbf6455dce94ea24d1a0b648fe4e7e45
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-08-28 18:10:29 +0200

    toevoegen nieuwe bestuur

--- lib/src/features/profiles/models/firestore_almanak_profile.dart
+++ lib/src/features/profiles/models/firestore_almanak_profile.dart
@@ -22,7 +22,17 @@ class FirestoreAlmanakProfile {
   final String? phonePrimary;
 
   bool get isAppCo => ['21203', '18031', '18257'].contains(identifier);
-  bool get isBestuur => bestuursFunctie != null;
+  bool get isBestuur => bestuursFunctie != null || [
+    '19147',
+    '19089',
+    '19212',
+    '19208',
+    '19221',
+    '18257',
+    '20085',
+    '19240',
+    '19280', 
+  ].contains(identifier);
 
   const FirestoreAlmanakProfile({
     required this.firstName,
