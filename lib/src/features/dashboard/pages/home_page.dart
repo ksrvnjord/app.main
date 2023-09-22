f99322067c8eafa2b18732b36b339c3a8a51ffd9
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-09-22 23:15:15 +0200

    formatting & linter issues

--- lib/src/features/dashboard/pages/home_page.dart
+++ lib/src/features/dashboard/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
-import 'package:ksrvnjord_main_app/assets/lustrum_colors.dart';
 import 'package:ksrvnjord_main_app/src/features/announcements/api/announcements.dart';
 import 'package:ksrvnjord_main_app/src/features/dashboard/api/vaarverbod_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/dashboard/widgets/announcements_widget.dart';
