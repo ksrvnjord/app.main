21b55235d48825c3c9d086e447e2482768d92d23
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-10-07 13:29:33 +0200

    add card padding to poll in single page

--- lib/src/features/polls/pages/poll_page.dart
+++ lib/src/features/polls/pages/poll_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/polls/api/polls_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/polls/widgets/poll_card.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
+import 'package:styled_widget/styled_widget.dart';
 
 class PollPage extends ConsumerWidget {
   // Constructor which takes a String pollId.
@@ -13,6 +14,7 @@ class PollPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final pollVal = ref.watch(pollProvider(pollId));
+    const double cardPadding = 8;
 
     return Scaffold(
       appBar: AppBar(
@@ -22,7 +24,7 @@ class PollPage extends ConsumerWidget {
         data: (poll) => PollCard(
           pollDoc: poll,
           isExpanded: true,
-        ),
+        ).padding(all: cardPadding),
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (error, stack) =>
             ErrorCardWidget(errorMessage: error.toString()),
