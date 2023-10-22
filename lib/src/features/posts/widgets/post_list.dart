b0a67c5357e0829bdda0e814050e303b150809b9
Alexander Serraris <alexander.cs.dev@gmail.com>
2023-10-22 17:32:45 +0200

    use pagination for posts

--- lib/src/features/posts/widgets/post_list.dart
+++ lib/src/features/posts/widgets/post_list.dart
@@ -1,40 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_pagination/firebase_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/api/posts_provider.dart';
+import 'package:ksrvnjord_main_app/src/features/posts/model/post.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_card.dart';
-import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
-import 'package:styled_widget/styled_widget.dart';
 
 class PostList extends ConsumerWidget {
   const PostList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final posts = ref.watch(postsProvider);
+    const docsPerQuery = 5;
 
-    return ListView(padding: const EdgeInsets.only(bottom: 64), children: [
-      posts.when(
-        data: (snapshot) => snapshot.size == 0
-            ? const Center(
-                child: Text("Wees de eerste die een post plaatst!"),
-              )
-            : [
-                for (final doc in snapshot.docs) ...[
-                  PostCard(
-                    snapshot: doc,
-                    elevation: false,
-                    squareBorder: true,
-                  ),
-                  const SizedBox(height: 8),
-                ],
-              ].toColumn(),
-        loading: () => const Center(
-          child: CircularProgressIndicator.adaptive(),
-        ),
-        error: (error, stack) => ErrorCardWidget(
-          errorMessage: error.toString(),
-        ),
-      ),
-    ]);
+    return FirestorePagination(
+      query: FirebaseFirestore.instance
+          .collection('posts')
+          .withConverter<Post>(
+            fromFirestore: (snapshot, _) =>
+                Post.fromJson(snapshot.data() ?? {}),
+            toFirestore: (post, _) => post.toJson(),
+          )
+          .orderBy('createdTime', descending: true),
+      itemBuilder: (context, snap, index) {
+        final postSnapshot = snap as DocumentSnapshot<Post>;
+
+        return PostCard(
+          snapshot: postSnapshot,
+          elevation: false,
+          squareBorder: true,
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(height: 8),
+      limit: docsPerQuery,
+      isLive: true,
+      padding: const EdgeInsets.only(bottom: 80),
+    );
   }
 }
