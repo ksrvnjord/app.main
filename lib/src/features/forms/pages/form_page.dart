cce164d43807ab7b12e4f1274dd7169fa06854df
PimVeefkind <pim.veefkind@gmail.com>
2024-01-02 17:40:26 +0100

    retract second bug fix

--- lib/src/features/forms/pages/form_page.dart
+++ lib/src/features/forms/pages/form_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:ksrvnjord_main_app/src/features/forms/api/can_edit_form_answer_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/forms/api/form_answer_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/forms/api/form_repository.dart';
 import 'package:ksrvnjord_main_app/src/features/forms/api/forms_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/forms/model/firestore_form.dart';
-import 'package:ksrvnjord_main_app/src/features/forms/widgets/delete_form_answer_button.dart';
 import 'package:ksrvnjord_main_app/src/features/forms/widgets/form_question.dart';
 import 'package:ksrvnjord_main_app/src/features/shared/widgets/error_card_widget.dart';
 import 'package:styled_widget/styled_widget.dart';
@@ -16,16 +15,8 @@ class FormPage extends ConsumerWidget {
 
   final String formId;
 
-  Future<void> deleteAnswerAndCloseDialog(
-    BuildContext context,
-    String answerPath,
-  ) async {
-    await FormRepository.deleteMyFormAnswer(answerPath);
-    // ignore: use_build_context_synchronously
-    Navigator.of(context).pop(true);
-  }
-
-  Future<bool?> deleteMyFormAnswer(
+  // ignore: avoid-dynamic
+  Future<dynamic> _deleteMyFormAnswer(
     WidgetRef ref,
     BuildContext context,
   ) async {
@@ -33,34 +24,36 @@ class FormPage extends ConsumerWidget {
       formAnswerProvider(formsCollection.doc(formId)).future,
     );
     if (answer.docs.isNotEmpty) {
-      final String answerPath = answer.docs.first.reference.path;
+      // ignore: avoid-unsafe-collection-methods
+      final answerPath = answer.docs.first.reference.path;
 
       // ignore: use_build_context_synchronously
       return showDialog(
         context: context,
-        builder: (context) => AlertDialog(
+        builder: (innerContext) => AlertDialog(
           title: const Text('Verwijderen'),
           content: const Text(
             'Weet je zeker dat je jouw form-reactie wilt verwijderen?',
           ),
           actions: [
             TextButton(
-              onPressed: () => Navigator.of(context).pop(),
+              onPressed: () => Navigator.of(innerContext).pop(),
               child: const Text('Annuleren'),
             ),
             TextButton(
-              onPressed: () => deleteAnswerAndCloseDialog(
-                context,
-                answerPath,
-              ),
+              // ignore: prefer-extracting-callbacks, avoid-passing-async-when-sync-expected
+              onPressed: () async {
+                await FormRepository.deleteMyFormAnswer(answerPath);
+                // ignore: use_build_context_synchronously
+                if (innerContext.mounted) Navigator.of(innerContext).pop(true);
+              },
               child: const Text('Verwijderen').textColor(Colors.red),
             ),
           ],
         ),
       );
-    } else {
-      throw Exception("Geen antwoord gevonden om te verwijderen");
     }
+    throw Exception("Geen antwoord gevonden om te verwijderen");
   }
 
   @override
@@ -70,9 +63,7 @@ class FormPage extends ConsumerWidget {
     final formVal = ref.watch(formProvider(doc));
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form'), // TODO: Misschien verwijderen.
-      ),
+      appBar: AppBar(title: const Text('Form')),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
@@ -85,11 +76,11 @@ class FormPage extends ConsumerWidget {
               }
               // Since the form exists, we can safely assume that the data is not null.
               // ignore: avoid-non-null-assertion
-              final FirestoreForm form = formDoc.data()!;
+              final form = formDoc.data()!;
 
-              final bool formIsOpen = DateTime.now().isBefore(form.openUntil);
+              final formIsOpen = DateTime.now().isBefore(form.openUntil);
 
-              const double descriptionVPadding = 16;
+              const descriptionVPadding = 16.0;
 
               final colorScheme = Theme.of(context).colorScheme;
 
@@ -101,36 +92,21 @@ class FormPage extends ConsumerWidget {
 
               final formKey = GlobalKey<FormState>();
 
-              // ignore: arguments-ordering
+              const sizedBoxHeight = 32.0;
+
               return [
-                Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      [
-                        Text(form.formName, style: textTheme.titleLarge),
-                      ].toRow(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      ),
-                      Text(
-                        '${formIsOpen ? "Sluit" : "Gesloten"} op ${DateFormat('EEEE d MMMM y HH:mm', 'nl_NL').format(form.openUntil)}',
-                        style: textTheme.bodySmall?.copyWith(
-                          color:
-                              formIsOpen ? Colors.green : colorScheme.outline,
-                        ),
-                      ),
-                      if (description != null)
-                        Text(description, style: textTheme.bodyMedium)
-                            .padding(vertical: descriptionVPadding),
-                    ],
+                [Text(form.formName, style: textTheme.titleLarge)]
+                    .toRow(mainAxisAlignment: MainAxisAlignment.spaceBetween),
+                Text(
+                  '${formIsOpen ? "Sluit" : "Gesloten"} op ${DateFormat('EEEE d MMMM y HH:mm', 'nl_NL').format(form.openUntil)}',
+                  style: textTheme.bodySmall?.copyWith(
+                    color: formIsOpen ? Colors.green : colorScheme.outline,
                   ),
-                  const Spacer(),
-                  DeleteFormAnswerButton(
-                    deleteMyFormAnswer: deleteMyFormAnswer,
-                    formId: formId,
-                    formIsOpen: formIsOpen,
-                  ),
-                ]),
+                ),
+                if (description != null)
+                  Text(description, style: textTheme.bodyMedium)
+                      .padding(vertical: descriptionVPadding),
+                const SizedBox(height: sizedBoxHeight),
                 Form(
                   key: formKey,
                   child: [
@@ -145,9 +121,7 @@ class FormPage extends ConsumerWidget {
                     ],
                   ].toColumn(),
                 ),
-              ].toColumn(
-                crossAxisAlignment: CrossAxisAlignment.start,
-              );
+              ].toColumn(crossAxisAlignment: CrossAxisAlignment.start);
             },
             error: (error, stack) =>
                 ErrorCardWidget(errorMessage: error.toString()),
@@ -156,6 +130,43 @@ class FormPage extends ConsumerWidget {
           ),
         ],
       ),
+      floatingActionButton: ref.watch(canRemoveFormAnswerProvider(doc)).when(
+            data: (canRemove) => canRemove
+                ? FloatingActionButton.extended(
+                    tooltip: "Verwijder mijn form reactie",
+                    foregroundColor:
+                        Theme.of(context).colorScheme.onErrorContainer,
+                    backgroundColor:
+                        Theme.of(context).colorScheme.errorContainer,
+                    heroTag: "delete-my-form-answer",
+                    // ignore: prefer-extracting-callbacks, avoid-passing-async-when-sync-expected
+                    onPressed: () async {
+                      final res = await _deleteMyFormAnswer(ref, context);
+                      if (res == true) {
+                        // ignore: use_build_context_synchronously, avoid-ignoring-return-values
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Jouw formreactie is verwijderd'),
+                          ),
+                        );
+                      } else {
+                        // ignore: use_build_context_synchronously, avoid-ignoring-return-values
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                              'Het is niet gelukt jouw formreactie te verwijderen',
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    icon: const Icon(Icons.delete),
+                    label: const Text("Verwijder mijn formreactie"),
+                  )
+                : const SizedBox.shrink(),
+            error: (err, stk) => const SizedBox.shrink(),
+            loading: () => const SizedBox.shrink(),
+          ),
     );
   }
 }
