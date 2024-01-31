b92ce7770d4dc6c315d70d8898634aabc140d14a
VincentvanRie <vincent.vanrie01@gmail.com>
2024-01-31 21:13:34 +0100

    Fix formatting in form_page.dart

--- lib/src/features/forms/pages/form_page.dart
+++ lib/src/features/forms/pages/form_page.dart
@@ -120,7 +120,8 @@ class _FormPageState extends ConsumerState<FormPage> {
                 return [
                   [
                     Flexible(
-                        child: Text(form.title, style: textTheme.titleLarge))
+                      child: Text(form.title, style: textTheme.titleLarge),
+                    )
                   ].toRow(mainAxisAlignment: MainAxisAlignment.spaceBetween),
                   Text(
                     '${formIsOpen ? "Sluit" : "Gesloten"} op ${DateFormat('EEEE d MMMM y HH:mm', 'nl_NL').format(openUntil)}',
