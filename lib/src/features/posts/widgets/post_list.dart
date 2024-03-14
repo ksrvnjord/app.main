999163c63a8cc3078303f03b7d048c3b515588ae
Alexander Serraris <alexander.cs.dev@gmail.com>
2024-03-14 10:56:16 +0100

    bohemian birds try-out

--- lib/src/features/posts/widgets/post_list.dart
+++ lib/src/features/posts/widgets/post_list.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_pagination/firebase_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/api/posts_provider.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/api/selected_topic_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/posts/model/post.dart';
 import 'package:ksrvnjord_main_app/src/features/posts/widgets/post_card.dart';
 
 class PostList extends ConsumerWidget {
@@ -11,21 +9,24 @@ class PostList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    const docsPerQuery = 5;
-
     final selectedTopic = ref.watch(selectedTopicProvider);
 
-    final query = ref.watch(postQueryProvider(selectedTopic));
+    final postsVal = ref.watch(postsProvider(selectedTopic));
 
-    return FirestorePagination<Post>(
-      query: query,
-      itemBuilder: (context, snapshot, index) => PostCard(
-        snapshot: snapshot,
-      ),
-      separatorBuilder: (context, index) => const SizedBox(height: 8),
-      limit: docsPerQuery,
-      isLive: true,
-      padding: const EdgeInsets.only(bottom: 80),
+    return postsVal.when(
+      data: (posts) {
+        return posts.docs.isEmpty
+            ? const Center(child: Text('No posts'))
+            : ListView(
+                padding: const EdgeInsets.only(bottom: 80),
+                children: [
+                  Image.asset('assets/images/sponsors/bohemian_birds.png'),
+                  for (final post in posts.docs) PostCard(snapshot: post),
+                ],
+              );
+      },
+      error: (error, _) => Center(child: Text('Error: $error')),
+      loading: () => const Center(child: CircularProgressIndicator.adaptive()),
     );
   }
 }
