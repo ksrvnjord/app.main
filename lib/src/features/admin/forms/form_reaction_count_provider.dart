18fcc66b673d0fd10430dbe26ac5cff0e071228d
Max Boone <contact@maxboone.com>
2024-05-19 21:18:03 +0000

    fix(flutter): update dependencies such that we can build with flutter 3.22.0

--- lib/src/features/admin/forms/form_reaction_count_provider.dart
+++ lib/src/features/admin/forms/form_reaction_count_provider.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // Provides a (partial) count of the reactions to a form.
 // It is partial because it includes also responses that have not completed all required questions.
 // ignore: prefer-static-class
-final formPartialReactionCountProvider = FutureProvider.family<int, String>(
+final formPartialReactionCountProvider = FutureProvider.family<int?, String>(
   (ref, docId) async {
     final snapshot = await FirebaseFirestore.instance
         .collection('forms')
