072cb8587fdf94ad9c70e416e40e31ada913436c
PimVeefkind <pim.veefkind@gmail.com>
2024-12-05 14:59:32 +0100

    upgrades to swipe between photos

--- lib/src/features/gallery/pages/gallery_file_page_view.dart
+++ lib/src/features/gallery/pages/gallery_file_page_view.dart
@@ -7,7 +7,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ksrvnjord_main_app/src/features/gallery/api/gallery_image_provider.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'dart:html' as html;
 
 class GalleryFilePageView extends ConsumerStatefulWidget {
   const GalleryFilePageView({
@@ -31,6 +33,31 @@ class _GalleryFilePageViewState extends ConsumerState<GalleryFilePageView> {
     super.initState();
     _currentPage = widget.initialIndex;
     _pageController = PageController(initialPage: widget.initialIndex);
+  }
+
+  void _preloadImages(int index) {
+    // Preload 5 images before and after the current page index.
+    for (int i = index - 5; i <= index + 5; i += 1) {
+      if (i >= 0 && i < widget.paths.length) {
+        final path = widget.paths[i];
+        final imageVal = ref.watch(galleryImageProvider(path.fullPath));
+        imageVal.when(
+          data: (image) {
+            // Preload the image using Image.memory.
+            precacheImage(MemoryImage(image.bytes), context);
+          },
+          error: (err, _) {},
+          loading: () {},
+        );
+      }
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Preload images around the current page once dependencies are resolved.
+    _preloadImages(_currentPage);
   }
 
   @override
@@ -62,7 +89,7 @@ class _GalleryFilePageViewState extends ConsumerState<GalleryFilePageView> {
                           XFile.fromData(
                             image.bytes,
                             mimeType: "image/jpeg",
-                            name: "foto_$_currentPage.jpg",
+                            name: "${path.fullPath}.jpg",
                           ),
                         ],
                         subject: "Foto",
@@ -76,6 +103,46 @@ class _GalleryFilePageViewState extends ConsumerState<GalleryFilePageView> {
             },
             icon: const Icon(Icons.share),
           ),
+          IconButton(
+            onPressed: () {
+              final path = widget.paths.elementAtOrNull(_currentPage);
+              if (path != null) {
+                final imageVal = ref.watch(galleryImageProvider(path.fullPath));
+
+                imageVal.when(
+                  data: (image) async {
+                    if (kIsWeb) {
+                      // Web-specific download logic.
+                      final blob = html.Blob([image.bytes]);
+                      final url = html.Url.createObjectUrlFromBlob(blob);
+                      final anchor = html.AnchorElement(href: url)
+                        ..target = 'blank'
+                        ..download = 'foto_$_currentPage.jpg';
+                      anchor.click();
+                      html.Url.revokeObjectUrl(url);
+                    } else {
+                      // Mobile or Desktop-specific download logic using XFile.
+                      final directory =
+                          await getApplicationDocumentsDirectory();
+                      final filePath =
+                          '${directory.path}/foto_$_currentPage.jpg';
+                      final xFile =
+                          XFile.fromData(image.bytes, mimeType: 'image/jpeg');
+                      await xFile.saveTo(filePath);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Image downloaded to $filePath'),
+                        ),
+                      );
+                    }
+                  },
+                  error: (err, _) {},
+                  loading: () {},
+                );
+              }
+            },
+            icon: const Icon(Icons.download),
+          ),
         ],
       ),
       body: GestureDetector(
@@ -84,6 +151,7 @@ class _GalleryFilePageViewState extends ConsumerState<GalleryFilePageView> {
           onPageChanged: (index) {
             setState(() {
               _currentPage = index;
+              _preloadImages(index);
             });
           },
           itemBuilder: (context, index) {
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -12,6 +12,7 @@
 #include <firebase_core/firebase_core_plugin_c_api.h>
 #include <firebase_storage/firebase_storage_plugin_c_api.h>
 #include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <sentry_flutter/sentry_flutter_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
@@ -28,6 +29,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("FirebaseStoragePluginCApi"));
   FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  SentryFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
