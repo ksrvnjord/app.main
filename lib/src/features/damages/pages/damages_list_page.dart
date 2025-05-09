6364cdc38353d7206a834e369937d427d46a6f94
PimVeefkind <pim.veefkind@gmail.com>
2025-05-09 12:23:24 +0200

    fix linter issues

--- lib/src/features/damages/pages/damages_list_page.dart
+++ lib/src/features/damages/pages/damages_list_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_pagination/firebase_pagination.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
