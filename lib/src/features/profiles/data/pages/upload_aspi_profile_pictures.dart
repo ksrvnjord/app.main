b499f136181e36f60f509f8cac682e31d0fe36d9
VincentvanRie <vincent.vanrie01@gmail.com>
2025-10-14 10:20:36 +0200

    Add loading picture asset and update image placeholders

--- lib/src/features/profiles/data/pages/upload_aspi_profile_pictures.dart
+++ lib/src/features/profiles/data/pages/upload_aspi_profile_pictures.dart
@@ -1,15 +1,9 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:desktop_drop/desktop_drop.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:ksrvnjord_main_app/assets/images.dart';
-import 'package:ksrvnjord_main_app/src/features/announcements/api/get_image.dart';
 import 'package:ksrvnjord_main_app/src/features/profiles/api/profile_picture_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/api/user_provider.dart';
-import 'package:ksrvnjord_main_app/src/features/profiles/edit_my_profile/models/profile_edit_form_notifier.dart';
 
 class UploadAspiProfilePictures extends StatelessWidget {
   const UploadAspiProfilePictures({super.key});
@@ -39,7 +33,7 @@ class UploadGridCell extends ConsumerStatefulWidget {
 }
 
 class _UploadGridCellState extends ConsumerState<UploadGridCell> {
-  bool _dragging = false;
+  final bool _dragging = false;
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
   Future<void> pickImage(
@@ -81,66 +75,54 @@ class _UploadGridCellState extends ConsumerState<UploadGridCell> {
     final colorScheme = Theme.of(context).colorScheme;
     final profilePicture = ref.watch(profilePictureProvider(lidnummer));
 
-    return DropTarget(
-      onDragEntered: (_) => setState(() => _dragging = true),
-      onDragExited: (_) => setState(() => _dragging = false),
-      onDragDone: (details) => (details) {
-        for (final file in details.files) {
-          // file is an XFile, use file.path to get the file path
-          // uploadFile(file.path);
-        }
-      },
-      child: GestureDetector(
-        onTap: () => pickImage(context, ref, lidnummer),
-        child: Container(
-          margin: const EdgeInsets.all(4),
-          decoration: BoxDecoration(
-            color: _dragging
-                ? colorScheme.onPrimaryContainer
-                : colorScheme.primaryContainer,
-            border: Border.all(color: colorScheme.primary),
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              Center(
-                child: Image(
-                  image: profilePicture.when(
-                    data: (imageProvider) => imageProvider,
-                    loading: () =>
-                        const AssetImage(Images.placeholderProfilePicture),
-                    error: (_, __) =>
-                        const AssetImage(Images.placeholderProfilePicture),
-                  ),
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) => Text(lidnummer),
+    return GestureDetector(
+      onTap: () => pickImage(context, ref, lidnummer),
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: _dragging
+              ? colorScheme.onPrimaryContainer
+              : colorScheme.primaryContainer,
+          border: Border.all(color: colorScheme.primary),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Center(
+              child: Image(
+                image: profilePicture.when(
+                  data: (imageProvider) => imageProvider,
+                  loading: () => const AssetImage(Images.loadingPicture),
+                  error: (_, __) =>
+                      const AssetImage(Images.placeholderProfilePicture),
                 ),
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => Text(lidnummer),
               ),
-              Center(
-                child: Container(
-                  color: const Color.fromARGB(185, 0, 0, 0),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-                  child: Text(
-                    lidnummer,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 2,
-                          color: Colors.black,
-                          offset: Offset(1, 1),
-                        ),
-                      ],
-                    ),
+            ),
+            Center(
+              child: Container(
+                color: const Color.fromARGB(185, 0, 0, 0),
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+                child: Text(
+                  lidnummer,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 2,
+                        color: Colors.black,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
