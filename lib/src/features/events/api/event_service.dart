632465bd6a0f86907f36387f7bfdc1e705e50f26
isisvanwolde <isisvanwolde@gmail.com>
2025-12-09 10:56:24 +0100

    linting fixes

--- lib/src/features/events/api/event_service.dart
+++ lib/src/features/events/api/event_service.dart
@@ -1,60 +1,6 @@
-// import 'dart:io';
-// import 'dart:ui';
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-// import 'package:firebase_storage/firebase_storage.dart';
-// import 'package:ksrvnjord_main_app/src/features/events/models/event.dart';
-// import 'package:ksrvnjord_main_app/src/features/profiles/models/user.dart';
-
-// class EventService {
-//   static final eventsCollection = FirebaseFirestore.instance
-//       .collection('events')
-//       // .withConverter<Event>(
-//       //   fromFirestore: (snapshot, _) => Event.fromJson(snapshot.data() ?? {}),
-//       //   toFirestore: (event, _) => event.toJson(),
-//       // );
-
-//   static deleteEvent(DocumentSnapshot<Event> snapshot) async {
-//     final event = snapshot.data();
-
-//     // if (event?.imageRef != null) {
-//     //   await event?.imageRef?.delete();
-//     // }
-//     snapshot.reference.delete();
-//   }
-
-//   static like(DocumentSnapshot<Event> snapshot) {
-//     final uid = firebase_auth.FirebaseAuth.instance.currentUser?.uid;
-//     final event = snapshot.data();
-//   }
-
-//   static Future<void> create({
-//     required String title,
-//     required String description,
-//     required Timestamp startTime,
-//     required Timestamp endTime,
-//     required Color color,
-//   }) async {
-//     final eventRef = eventsCollection.doc(); // Make a new document reference.
-
-//     await eventRef.set(
-//       Event(
-//         title: title,
-//         description: description,
-//         startTime: startTime,
-//         endTime: endTime,
-//         color: color,
-//       ),
-//     );
-
-//   }
-// }
-
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:ksrvnjord_main_app/src/features/events/models/event.dart';
 
 class EventService {
