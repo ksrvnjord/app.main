98247f571d1f2c96c2be4e09e4248051c66588e8
FinnSchuirink <s4065328@vuw.leidenuniv.nl>
2026-06-20 17:03:44 +0200

    Added huis

--- lib/src/features/profiles/substructures/api/vertical_info_provider.dart
+++ lib/src/features/profiles/substructures/api/vertical_info_provider.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
-// A provider that fetches the substructure info for a given substructure name from firebase
+// A provider that fetches the verticaal info for a given verticaal name from firebase
 Future<String?> fetchDescription(String name) async {
   final filePath = '/almanak/verticals/$name/${name}Omschrijving.txt';
   final storageRef = FirebaseStorage.instance.ref(filePath);
